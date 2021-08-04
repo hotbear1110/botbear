02f54083530ea7a267aff99cc2e2d48d65e9c788
hotbear1110 <mads095h@gmail.com>
2021-08-04 19:37:31 +0200

    Added connect module

---
+++ Connect/connect.js
@@ -0,0 +1,49 @@
+require('dotenv').config()
+const mysql = require("mysql2");
+
+console.log(process.env.DB_HOST)
+
+let client = {
+    username: process.env.TWITCH_USER,
+    password: process.env.TWITCH_PASSWORD,
+};
+
+
+const con = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+});
+
+
+const getChannels = () =>
+  new Promise((resolve, reject) => {
+    con.query("SELECT * FROM Streamers", (err, results) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(results);
+      }
+    });
+  });
+
+
+
+const channelList = [];
+const channelOptions = [];
+async function channels() {
+  channelList.push(await getChannels());
+  await channelList[0].forEach((i) => {
+    channelOptions.push(i.username);
+  });
+  return channelOptions
+}
+
+console.log(channels())
+
+module.exports = {
+    con, 
+    client,
+    channels: channels
+ }
