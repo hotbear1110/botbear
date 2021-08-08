32d7645d94c17e7368fc48602312844f8d359dbe
hotbear1110 <mads095h@gmail.com>
2021-08-08 12:55:37 +0200

    Added notify game and title

--- commands/remove.js
+++ commands/remove.js
@@ -7,14 +7,14 @@ module.exports = {
         try {
             switch (input[2]) {
                 case "live":
-                    const getUsers = await tools.query(`SELECT * FROM Streamers WHERE username="${data.channelName}"`)
-                    let users = JSON.parse(getUsers[0].ping_users)
+                    const liveUsers = await tools.query(`SELECT * FROM Streamers WHERE username="${data.channelName}"`)
+                    let liveusers = JSON.parse(liveUsers[0].live_ping)
 
-                    if (users.includes(data.senderUsername)) {
-                        users.splice(users.indexOf(data.senderUsername), 1);
-                        users = JSON.stringify(users)
+                    if (liveusers.includes(data.senderUsername)) {
+                        liveusers.splice(liveusers.indexOf(data.senderUsername), 1);
+                        liveusers = JSON.stringify(liveusers)
 
-                        tools.query(`UPDATE Streamers SET ping_users=? WHERE username=?`, [users, data.channelName])
+                        tools.query(`UPDATE Streamers SET live_ping=? WHERE username=?`, [liveusers, data.channelName])
 
                         return `You are now unsubscribed from the event "live"`;
                     }
@@ -23,12 +23,38 @@ module.exports = {
                     }
                     break;
                 case "title":
-                    // remove user from title notify list
+                    const titleUsers = await tools.query(`SELECT * FROM Streamers WHERE username="${data.channelName}"`)
+                    let titleusers = JSON.parse(titleUsers[0].title_ping)
+
+                    if (titleusers.includes(data.senderUsername)) {
+                        titleusers.splice(titleusers.indexOf(data.senderUsername), 1);
+                        titleusers = JSON.stringify(titleusers)
+
+                        tools.query(`UPDATE Streamers SET title_ping=? WHERE username=?`, [titleusers, data.channelName])
+
+                        return `You are now unsubscribed from the event "title"`;
+                    }
+                    else {
+                        return 'You do not have a subscription for the event "title". If you want to subscribe, type "bb notify title".';
+                    }
                     break;
                 case "game":
-                // remove user from gamenotify list
+                    const gameUsers = await tools.query(`SELECT * FROM Streamers WHERE username="${data.channelName}"`)
+                    let gameusers = JSON.parse(gameUsers[0].game_ping)
+
+                    if (gameusers.includes(data.senderUsername)) {
+                        gameusers.splice(gameusers.indexOf(data.senderUsername), 1);
+                        gameusers = JSON.stringify(gameusers)
+
+                        tools.query(`UPDATE Streamers SET game_ping=? WHERE username=?`, [gameusers, data.channelName])
+
+                        return `You are now unsubscribed from the event "game"`;
+                    }
+                    else {
+                        return 'You do not have a subscription for the event "game". If you want to subscribe, type "bb notify game".';
+                    }
                 default:
-                    return `Please specify an event to un-subscribe to. The following events are available: live (might add more stuff later) `;
+                    return `Please specify an event to un-subscribe to. The following events are available: live, title, game`;
             }
         } catch (err) {
             console.log(err);
