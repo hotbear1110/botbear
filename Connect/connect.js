ff9b91c9dbf26e5477d0346b4e5230b495b4ac20
hotbear1110 <mads095h@gmail.com>
2021-08-08 13:14:44 +0200

    Update loops.js

---
+++ Connect/connect.js
@@ -0,0 +1,47 @@
+require('dotenv').config()
+const mysql = require("mysql2");
+
+
+let client = {
+  username: process.env.TWITCH_USER,
+  password: process.env.TWITCH_PASSWORD,
+  rateLimits: "default",
+};
+
+
+const con = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+
+const getChannels = () =>
+  new Promise((resolve, reject) => {
+    con.query("SELECT * FROM Streamers", (err, results) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(results);
+      }
+    });
+  });
+
+
+
+const channelList = [];
+const channelOptions = [];
+async function channels() {
+  channelList.push(await getChannels());
+  await channelList[0].forEach((i) => {
+    channelOptions.push(i.username);
+  });
+  return channelOptions
+}
+
+module.exports = {
+  con,
+  client,
+  channels: channels
+}
---
+++ loops/loops.js
@@ -0,0 +1,140 @@
+require('dotenv').config()
+const { ChatClient } = require("dank-twitch-irc");
+const login = require('../connect/connect.js');
+const tools = require("../tools/tools.js")
+const _ = require("underscore")
+const axios = require('axios');
+
+const cc = new ChatClient(login.client);
+
+
+
+cc.on("ready", async () => {
+    console.log("Loop works yes!")
+    cc.joinAll(await login.channels())
+
+});
+
+
+
+
+cc.connect();
+
+setInterval(async function () {
+    const streamers = await tools.query('SELECT * FROM Streamers')
+
+    _.each(streamers, async function (stream) {
+        await axios.get(`https://api.twitch.tv/helix/streams?user_login=${stream.username}`, {
+            headers: {
+                'client-id': process.env.TWITCH_CLIENTID,
+                'Authorization': process.env.TWITCH_AUTH
+            }
+        })
+            .then(async function (response) {
+                // handle success
+                const twitchdata = response.data;
+                let users = JSON.parse(stream.live_ping)
+                users = users.toString().replaceAll(',', ' ')
+
+
+                let userlist = tools.splitLine(users, 350)
+
+                if (twitchdata['data'].length !== 0 && stream.islive == 0) {
+                    console.log(stream.username + " IS NOW LIVE");
+                    await tools.query(`UPDATE Streamers SET islive = 1 WHERE username = "${stream.username}"`)
+                    _.each(userlist, function (msg, i) {
+                        setTimeout(function () {
+                            cc.me(`${stream.username}`, `${stream.liveemote} ${stream.username} IS NOW LIVE ${stream.liveemote} ${userlist[i]}`);
+                        }, 2000 * i);
+                    });
+                };
+                if (twitchdata['data'].length === 0 && stream.islive == 1) {
+                    console.log(stream.username + " IS NOW OFFLINE");
+                    await tools.query(`UPDATE Streamers SET islive = 0 WHERE username ="${stream.username}"`)
+                    _.each(userlist, function (msg, i) {
+                        setTimeout(function () {
+                            cc.me(`${stream.username}`, `${stream.offlineemote} ${stream.username} IS NOW OFFLINE ${stream.offlineemote} ${userlist[i].toString().replaceAll(',', ' ')}`);
+                        }, 2000 * i);
+                    });
+                };
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .then(function () {
+                // always executed
+            });
+    })
+    _.each(streamers, async function (stream) {
+        await axios.get(`https://api.twitch.tv/helix/channels?broadcaster_id=${stream.uid}`, {
+            headers: {
+                'client-id': process.env.TWITCH_CLIENTID,
+                'Authorization': process.env.TWITCH_AUTH
+            }
+        })
+            .then(async function (response) {
+                // handle success
+                const twitchdata = response.data;
+                let newTitle = twitchdata.data[0].title
+                let users = JSON.parse(stream.title_ping)
+                users = users.toString().replaceAll(',', ' ')
+
+
+                let userlist = tools.splitLine(users, 350)
+
+                if (newTitle != stream.title) {
+                    console.log(stream.username + " NEW TITLE: " + newTitle);
+                    await tools.query(`UPDATE Streamers SET title=? WHERE username=?`, [newTitle, stream.username])
+                    _.each(userlist, function (msg, i) {
+                        setTimeout(function () {
+                            cc.me(`${stream.username}`, `${stream.liveemote} NEW TITLE ! ${stream.liveemote} 👉 ${newTitle} 👉 ${userlist[i]}`);
+                        }, 2000 * i);
+                    });
+                };
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .then(function () {
+                // always executed
+            });
+    })
+    _.each(streamers, async function (stream) {
+        await axios.get(`https://api.twitch.tv/helix/channels?broadcaster_id=${stream.uid}`, {
+            headers: {
+                'client-id': process.env.TWITCH_CLIENTID,
+                'Authorization': process.env.TWITCH_AUTH
+            }
+        })
+            .then(async function (response) {
+                // handle success
+                const twitchdata = response.data;
+                let newGame = twitchdata.data[0].game_name
+                let users = JSON.parse(stream.game_ping)
+                users = users.toString().replaceAll(',', ' ')
+
+
+                let userlist = tools.splitLine(users, 350)
+
+                if (newGame != stream.game) {
+                    console.log(stream.username + " NEW GAME: " + newGame);
+                    await tools.query(`UPDATE Streamers SET game=? WHERE username=?`, [newGame, stream.username])
+                    _.each(userlist, function (msg, i) {
+                        setTimeout(function () {
+                            cc.me(`${stream.username}`, `${stream.liveemote} NEW GAME ! ${stream.liveemote} 👉 ${newGame} 👉 ${userlist[i]}`);
+                        }, 2000 * i);
+                    });
+                };
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .then(function () {
+                // always executed
+            });
+    })
+
+}, 10000)
