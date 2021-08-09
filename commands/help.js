1d2f91480d2bbec7763ee256b2e27d1d1eb9357b
hotbear1110 <mads095h@gmail.com>
2021-08-09 22:08:13 +0200

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: "help",
+    execute: async (channel, user, input) => {
+        try {
+            return `nymnDank botbear1110 is a scuffed bot, that works a bit like titlechange bot rn, for the future i would like the bot to be an all purpose bot. The bot is written entirely in Node.js and is made by HotBear1110 Link to my GitHub: https://github.com/hotbear1110/botbear List of commands: https://botbear.github.io/`;
+        } catch (err) {
+            console.log(err);
+            return ` Error FeelsBadMan `;
+        }
+    }
+}
