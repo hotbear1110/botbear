48213bce31ef5a166a8447124b6cc63e3705d8c5
hotbear1110 <mads095h@gmail.com>
2021-08-12 19:43:28 +0200

    Added a suggest command

--- commands/say.js
+++ commands/say.js
@@ -1,16 +1,25 @@
 const cc = require("../bot.js").cc;
+const tools = require("../tools/tools.js")
 module.exports = {
     name: "say",
     execute: async (channel, user, input) => {
         try {
+            input.splice(0,1)
             let msg = input.toString();
             msg = msg.substring(7)
             msg = msg.toString().replaceAll(',', ' ')
-            
+
             if (user.username != "hotbear1110") {
                 return;
             }
             else {
+                const banPhrase = await tools.banphrasePass(msg, channel);
+
+                if (banPhrase.banned) {
+                    cc.say(channel, `[Banphrased] cmonBruh `);
+                    return;
+                }
+
                 cc.say(`#${channel}`, msg)
             }
         } catch (err) {
---
+++ commands/suggest.js
@@ -0,0 +1,19 @@
+const tools = require("../tools/tools.js");
+
+module.exports = {
+    name: "suggest",
+    execute: async (channel, user, input) => {
+        try {
+            const IDs = await tools.query(`SELECT * FROM Suggestions`)
+            let newID = IDs.length + 1;
+            input = input.splice(2)
+
+            let msg = input.toString().replaceAll(',', ' ')
+            await tools.query('INSERT INTO Suggestions (ID, User, Suggestion) values (?, ?, ?)', [newID, user.username, msg]);
+            return `Your suggestion was saved as 'ID ${newID}' nymnDank 👍 `
+        } catch (err) {
+            console.log(err);
+            return ` Error FeelsBadMan `;
+        }
+    }
+}
