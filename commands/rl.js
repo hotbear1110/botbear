69fa7024777d11483fa7942aae69d81fea8721a6
hotbear1110 <mads095h@gmail.com>
2021-08-14 12:55:42 +0200

    Update rl.js

--- commands/rl.js
+++ commands/rl.js
@@ -10,10 +10,12 @@ module.exports = {
             }
 
             const rl = await got(`https://api.ivr.fi/logs/rq/${channel}/${username}`).json();
-            
-            resolve(`(#${channel}) ${rl.user}: ${rl.message} (${rl.time} ago)`)
-            return `(#${channel}) ${rl.user}: ${rl.message} (${rl.time} ago)`
-    
+
+            if (rl.status !== 404) {
+                resolve(`(#${channel}) ${rl.user}: ${rl.message} (${rl.time} ago)`)
+                return `(#${channel}) ${rl.user}: ${rl.message} (${rl.time} ago)`
+            }
+
         } catch (err) {
             console.log(err);
             return ` Error FeelsBadMan : This chat does not have logs on: https://logs.ivr.fi/`;
