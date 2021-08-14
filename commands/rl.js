e57bf1b8f2326ffc5525d155291184b9d6a39ef4
hotbear1110 <mads095h@gmail.com>
2021-08-14 12:50:48 +0200

    Update rl.js

--- commands/rl.js
+++ commands/rl.js
@@ -2,7 +2,7 @@ const got = require("got");
 
 module.exports = {
     name: "rl",
-    execute: async (channel, user, input) => {
+    execute: async (channel, user, input) => new Promise(async (resolve, reject) => {
         try {
             let username = user.username;
             if (input[3]) {
@@ -10,12 +10,13 @@ module.exports = {
             }
 
             const rl = await got(`https://api.ivr.fi/logs/rq/${channel}/${username}`).json();
-
+            
+            resolve(`(#${channel}) ${rl.user}: ${rl.message} (${rl.time} ago)`)
             return `(#${channel}) ${rl.user}: ${rl.message} (${rl.time} ago)`
     
         } catch (err) {
             console.log(err);
             return ` Error FeelsBadMan `;
         }
-    }
+    })
 }
