acbe54e06cbf0d87a491bb5d8699214d372ffff7
hotbear1110 <mads095h@gmail.com>
2021-08-14 12:46:55 +0200

    Create rl.js

---
+++ commands/rl.js
@@ -0,0 +1,21 @@
+const got = require("got");
+
+module.exports = {
+    name: "rl",
+    execute: async (channel, user, input) => {
+        try {
+            let username = user.username;
+            if (input[3]) {
+                username = input[3];
+            }
+
+            const rl = await got(`https://api.ivr.fi/logs/rq/${channel}/${username}`).json();
+
+            return `(#${channel}) ${rl.user}: ${rl.message} (${rl.time} ago)`
+    
+        } catch (err) {
+            console.log(err);
+            return ` Error FeelsBadMan `;
+        }
+    }
+}
