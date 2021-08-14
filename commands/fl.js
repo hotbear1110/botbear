9647b6b0b219d5d5a25136ccd73fa3744144cafe
hotbear1110 <mads095h@gmail.com>
2021-08-14 13:14:53 +0200

    Added a first line command

---
+++ commands/fl.js
@@ -0,0 +1,24 @@
+const got = require("got");
+const cc = require("../bot.js").cc;
+
+module.exports = {
+    name: "fl",
+    execute: async (channel, user, input) => {
+        try {
+            let username = user.username;
+            if (input[2]) {
+                username = input[2];
+            }
+
+            const fl = await got(`https://api.ivr.fi/logs/firstmessage/${channel}/${username}`).json();
+
+            if (fl.status !== 404) {
+                cc.say(`#${channel}`, `FeelsDankMan ${fl.user}'s first message in #${channel} was: ${fl.message} - (${fl.time} ago)`)
+            }
+
+        } catch (err) {
+            console.log(err);
+            return ` Error FeelsBadMan : No logs for that user in this chat`;
+        }
+    }
+}
