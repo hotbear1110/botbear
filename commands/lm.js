12311cd5dc109b71672616e45079694adf06c9bf
hotbear1110 <mads095h@gmail.com>
2021-08-16 00:17:29 +0200

    Added a last message command

---
+++ commands/lm.js
@@ -0,0 +1,25 @@
+const got = require("got");
+const cc = require("../bot.js").cc;
+
+module.exports = {
+    name: "lm",
+    ping: false,
+    execute: async (channel, user, input) => {
+        try {
+            let username = user.username;
+            if (input[2]) {
+                username = input[2];
+            }
+
+            const lm = await got(`https://api.ivr.fi/logs/lastmessage/${channel}/${username}`).json();
+
+            if (lm.status !== 404) {
+                cc.say(`#${channel}`, `FeelsDankMan ${lm.user}'s last message here was: ${lm.response} - (${lm.time} ago)`)
+            }
+
+        } catch (err) {
+            console.log(err);
+            return ` Error FeelsBadMan : No logs for that user in this chat`;
+        }
+    }
+}
