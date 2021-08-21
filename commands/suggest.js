0066095f647058166ff1bd7ed2d7ac99fda578fa
hotbear1110 <mads095h@gmail.com>
2021-08-21 12:42:09 +0200

    Update suggest.js

--- commands/suggest.js
+++ commands/suggest.js
@@ -12,8 +12,9 @@ module.exports = {
             await tools.query('INSERT INTO Suggestions (User, Suggestion) values (?, ?)', [user.username, msg]);
             
             const IDs = await tools.query(`SELECT MAX(ID) FROM Suggestions WHERE User=?`, [user.username])
+            console.log(IDs[0]['MAX(ID)'])
             
-            return `Your suggestion was saved as 'ID ${IDs}' nymnDank 👍 `
+            return `Your suggestion was saved as 'ID ${IDs[0]['MAX(ID)']}' nymnDank 👍 `
         } catch (err) {
             console.log(err);
             return ` Error FeelsBadMan `;
