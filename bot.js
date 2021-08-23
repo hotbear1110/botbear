bb4fdea2319c8a7618e035d414f6aebf95e9d574
hotbear1110 <mads095h@gmail.com>
2021-08-23 22:19:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ async function onMessageHandler(channel, user, msg, self) {
 
     const userList = await tools.query(`SELECT * FROM Users WHERE username=?`, [user.username])
 
-    if (!userList.length) {
+    if (!userList.length && user.username != null) {
         await tools.query('INSERT INTO Users (username, uid, permission) values (?, ?, ?)', [user.username, user["user-id"], 100]);
     }
 
