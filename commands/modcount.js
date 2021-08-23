82ab85111621f9f56e5d46937801c1b710b54a00
hotbear1110 <mads095h@gmail.com>
2021-08-23 23:48:57 +0200

    Added a modcount command

---
+++ commands/modcount.js
@@ -0,0 +1,31 @@
+const axios = require('axios');
+const _ = require("underscore")
+const tools = require("../tools/tools.js");
+
+
+module.exports = {
+    name: "modcheck",
+    ping: true,
+    execute: async (channel, user, input) => {
+        try {
+            let username = user.username;
+            if (input[2]) {
+                if (input[2].startsWith("@")) {
+                    input[2] = input[2].substring(1)
+                }
+                username = input[2];
+            }
+            let modcount = await axios.get(`https://modlookup.3v.fi/api/user-totals/${username}`)
+            console.log(modcount.data)
+            console.log(modcount.data.total)
+            ismod = modcount.data["total"]
+            if (ismod === 0) {
+                return `That user is not a mod in any channel :)`
+            }
+            return `That user is a m OMEGALUL d in ${ismod} channel('s)`
+        } catch (err) {
+            console.log(err);
+            return ` Error FeelsBadMan `;
+        }
+    }
+}
