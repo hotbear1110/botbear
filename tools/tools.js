0ede9ddb680795b16cb592bd5da541453c717d6d
hotbear1110 <mads095h@gmail.com>
2021-08-24 18:00:29 +0200

    Update tools.js

--- tools/tools.js
+++ tools/tools.js
@@ -31,7 +31,7 @@ exports.banphrasePass = (message, channel) => new Promise(async (resolve, reject
         [this.channel]);
         this.banphraseapi = this.data[0].banphraseapi;
     try {
-        if (this.banphraseapi === null) {
+        if (!this.banphraseapi) {
             this.banphraseapi = "https://pajlada.pajbot.com/api/v1/banphrases/test"
             return;
         }
