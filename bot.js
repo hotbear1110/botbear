bc4346051661b7d6af58d0f56e0d85d376b4a569
hotbear1110 <mads095h@gmail.com>
2021-08-30 23:39:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -154,7 +154,7 @@ async function onMessageHandler(channel, user, msg, self) {
         return;
     }
     if (result === oldmessage) {
-        result = `${result}ﾠ`
+        result = `${result}​`
     }
 
     cc.say(channel, result);
