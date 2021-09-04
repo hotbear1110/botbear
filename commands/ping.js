5e77abcb29442a7c9fddf2ffea58f0190b4bd354
hotbear1110 <mads095h@gmail.com>
2021-09-04 21:09:27 +0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -6,6 +6,7 @@ module.exports = {
     execute: async (channel, user, input, perm) => {
         try {
             const latency = await tools.query(`SELECT Latency FROM Latency ORDER BY id DESC LIMIT 1`);
+            let delay = JSON.parse(latency[0].Latency);
 
             return `nymnDank pong! - ${latency}s`;
         } catch (err) {
