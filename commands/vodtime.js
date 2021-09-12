ad661f0b442186c4ea6c14278fce0ae56683a787
hotbear1110 <mads095h@gmail.com>
2021-09-12 13:19:14 +0200

    Fixed so that the vodtime command, works with streams that are on 2 different dates at once

--- commands/vodtime.js
+++ commands/vodtime.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const axios = require('axios');
 const tools = require("../tools/tools.js");
+const date = require('date-and-time');
 
 module.exports = {
     name: "vodtime",
@@ -8,15 +9,8 @@ module.exports = {
     execute: async (channel, user, input, perm) => {
         try {
             let vodid = input[2].split('/');
-            console.log(vodid)
             vodid = vodid[vodid.length - 1];
-            console.log(vodid)
-            let CETfix = input[3].split(":")
-            let CEToffset = CETfix[0] - 2
-            if (CEToffset < 10) {
-                CEToffset = `0${CEToffset}`
-            }
-            CETfix = `${CEToffset}:${CETfix[1]}`
+
 
             const response = await axios.get(`https://api.twitch.tv/helix/videos?id=${vodid}`, {
                 headers: {
@@ -25,15 +19,73 @@ module.exports = {
                 }
             })
 
-            console.log(response.data.data[0].created_at)
             let starttime = response.data.data[0].created_at;
+
             let giventime = starttime.split("T");
-            giventime = `${giventime[0]}T${CETfix}:00Z`
-            console.log(Date.parse(giventime))
+            giventime = `${giventime[0]}T${input[3]}:00Z`;
+            giventime = new Date(giventime);
+
+            starttime = new Date(starttime);
+            starttime = date.addHours(starttime, 2);
+
+
+            let duration = response.data.data[0].duration;
+            console.log(duration)
+            duration = duration.split("h");
+
+            let hours = duration[0];
+
+            duration = duration[1].split("m");
+
+            let minutes = duration[0];
+
+            let seconds = duration[1];
+
+            seconds = seconds.replaceAll("s", "");
+
+            let endtime = date.addHours(starttime, hours);
+            endtime = date.addMinutes(endtime, minutes);
+            endtime = date.addSeconds(endtime, seconds);
+
+            let newgiventime = false;
+            let otherdate = false;
+            console.log(date.subtract(endtime, starttime).toDays())
+
+            if (date.subtract(endtime, starttime).toDays() > 0) {
+                newgiventime = date.addDays(giventime, 1);
+                newgiventime = newgiventime.toString().split("T");
+                newgiventime = newgiventime[0];
+
+                let newgiventime2 = giventime.toString().split("T");
+                newgiventime2 = newgiventime2[1];
+
+                newgiventime = `${newgiventime}T${newgiventime2}`;
+            }
+
+            if (Date.parse(giventime) > Date.parse(endtime)) {
+                return `${input[3]} is after stream end.`;
+            }
+
+            if (newgiventime !== false) {
+                if (Date.parse(newgiventime) > Date.parse(endtime) && Date.parse(giventime) > Date.parse(endtime)) {
+                    return `${input[3]} is after stream end.`;
+                }
+                if (Date.parse(giventime) < Date.parse(starttime) && Date.parse(newgiventime) > Date.parse(endtime)) {
+                    return `${input[3]} is before stream start or after stream end.`;
+
+                }
+                if (Date.parse(giventime) < Date.parse(starttime)) {
+                    giventime = newgiventime
+                }
+            } else if (Date.parse(giventime) < Date.parse(starttime)) {
+                return `${input[3]} is before stream start.`;
+            }
+
+
+
 
             const ms = Date.parse(giventime) - Date.parse(starttime)
 
-            console.log(tools.humanizeDuration(ms))
 
             let timestamp = tools.humanizeDuration(ms);
             timestamp = timestamp.replaceAll(",", "");
