90b5eb016ed8365793930f59fd31ed6bbb88fe72
hotbear1110 <mads095h@gmail.com>
2021-09-17 01:52:01 +0200

    Update bmi.js

--- commands/bmi.js
+++ commands/bmi.js
@@ -3,8 +3,8 @@ module.exports = {
     ping: true,
     execute: async (channel, user, input, perm) => {
         try {
-            let bmi = input[1]/(input[2]**2)
-            return bmi;
+            let bmi = (input[3]**2)/10000
+            return input[2]/bmi
         } catch (err) {
             console.log(err);
             return ` Error FeelsBadMan `;
