b6793b6a34a9a6a2bc21cc437b609e6640d5adf0
hotbear1110 <mads095h@gmail.com>
2021-09-18 14:26:09 +0200

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,13 +1,12 @@
+const cc = require("../bot.js").cc;
+
 module.exports = {
     name: "test",
     ping: true,
     execute: async (channel, user, input, perm) => {
         try {
-            let Time = new Date().getTime();
-            let RemindTime = Time + 7200000;
-
-
-            return RemindTime;
+            console.log(user.username)
+            cc.whisper(user.username, "test")
         } catch (err) {
             console.log(err);
             return ` Error FeelsBadMan `;
