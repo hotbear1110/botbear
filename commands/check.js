207149664b7afc927abe869455d53d1445895aa2
hotbear1110 <mads095h@gmail.com>
2021-09-20 21:43:12 +0200

    Added a command for checking information about users

---
+++ commands/check.js
@@ -0,0 +1,34 @@
+const tools = require("../tools/tools.js");
+
+module.exports = {
+    name: "check",
+    ping: true,
+    description: "Check for information about other users",
+    permission: 100,
+    execute: async (channel, user, input, perm) => {
+        try {
+            if (this.permission > perm) {
+                return;
+            }
+            switch (input[2]) {
+                case "permission":
+                    let username = user.username;
+                    if (input[3]) {
+                        if (input[3].startsWith("@")) {
+                            input[3] = input[3].substring(1);
+                        }
+                        username = input[3];
+                    }
+                    const User = await tools.query(`SELECT permission FROM Users WHERE username=?`, [username]);
+
+                    console.log(User)
+                    return `${username}'s permission is: ${User[0].permission}`;
+
+                default: `Stuff available to check: permission`;
+            }
+        } catch (err) {
+            console.log(err);
+            return ` Error FeelsBadMan `;
+        }
+    }
+}
