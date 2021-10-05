d27ef7b4509d29330407198e4378ff248009fa01
hotbear1110 <mads095h@gmail.com>
2021-10-05 20:23:02 +0200

    Added a oldestvod command

---
+++ commands/oldestvod.js
@@ -0,0 +1,51 @@
+const axios = require('axios');
+
+module.exports = {
+    name: "oldestvod",
+    ping: true,
+    description: 'This command will give you a link to the oldest available twitch vod for a given channel',
+    permission: 100,
+    category: "Info command",
+    execute: async (channel, user, input, perm) => {
+        try {
+            if (module.exports.permission > perm) {
+                return;
+            }
+            let realchannel = channel;
+
+            if (input[2]) {
+                if (input[2].startsWith("@")) {
+                    input[2] = input[2].substring(1);
+                }
+                realchannel = input[2];
+            }
+
+            const userID = await axios.get(`https://api.ivr.fi/twitch/resolve/${realchannel}`, { timeout: 10000 });
+
+            if (userID.status === 404) {
+                return `Could not find user: "${realchannel}"`;
+            }
+
+            let vodList = await axios.get(`https://api.twitch.tv/helix/videos?user_id=${userID.data.id}&type=archive&first=100`, {
+                headers: {
+                    'client-id': process.env.TWITCH_CLIENTID,
+                    'Authorization': process.env.TWITCH_AUTH
+                },
+                timeout: 10000
+            });
+
+            vodList = vodList.data
+
+            if (!vodList.data.length) {
+                return `That channel has no vods`;
+            } else if (!vodList.data[vodNumber]) {
+                return `That channel only has ${vodList.data.length} vod(s)`;
+            } else {
+                return `${vodList.data[vodList.data.length].url}?t=0s`;
+            }
+        } catch (err) {
+            console.log(err);
+            return ` Error FeelsBadMan `;
+        }
+    }
+}
