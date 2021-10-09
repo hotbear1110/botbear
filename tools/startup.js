bdece02cbc8a92ac673634dba79261cfb2668307
hotbear1110 <mads095h@gmail.com>
2021-10-09 16:22:13 +0200

    Update startup.js

--- tools/startup.js
+++ tools/startup.js
@@ -9,12 +9,12 @@ let bannedUsers = await tools.bannedStreamer;
 
 if (await bannedUsers.length) {
     _.each(bannedUsers, async function (user) {
-        cc.part(user).then((data) => {
+        await cc.part(user).then((data) => {
             // data returns [channel]
         }).catch((err) => {
             console.log(err);
         });
-        cc.say("#botbear1110", `Left channel ${user}. Reason: Banned/deleted channel`)
+        await cc.say("#botbear1110", `Left channel ${user}. Reason: Banned/deleted channel`)
     })
 }
 
@@ -22,22 +22,22 @@ let namechange = await tools.nameChanges;
 
 if (await namechange.length) {
     _.each(namechange, async function (name) {
-        cc.join(name[0]).then((data) => {
+        await cc.join(name[0]).then((data) => {
             // data returns [channel]
         }).catch((err) => {
             console.log(err);
         });
 
-        cc.part(name[1]).then((data) => {
+        await cc.part(name[1]).then((data) => {
             // data returns [channel]
         }).catch((err) => {
             console.log(err);
         });
 
-        cc.say(`#${name[0]}`, `Name change detected, ${name[1]} -> ${name[0]}`)
-        cc.say("#botbear1110", `Left channel ${name[1]}. Reason: Name change detected, ${name[1]} -> ${name[0]}`)
+        await cc.say(`#${name[0]}`, `Name change detected, ${name[1]} -> ${name[0]}`)
+        await cc.say("#botbear1110", `Left channel ${name[1]}. Reason: Name change detected, ${name[1]} -> ${name[0]}`)
     })
 }
 }
 
-start()
+await start()
