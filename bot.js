bc3379da874c14fcd2f37a39da7fd07a5501f9dc
hotbear1110 <mads095h@gmail.com>
2021-10-09 20:31:07 +0200

    Revert "github is weird so i have to push this :/"
    This reverts commit 2571c98502e7017d7305a57197a46a4d8810d56f.

--- bot.js
+++ bot.js
@@ -200,9 +200,39 @@ async function onConnectedHandler(addr, port) {
 
     await tools.refreshCommands();
 
-    await tools.nameChanges();
+let bannedUsers = await tools.bannedStreamer();
 
-    await tools.bannedStreamer();
+if (await bannedUsers.length) {
+_.each(bannedUsers, async function (user) {
+    cc.part(user).then((data) => {
+        // data returns [channel]
+    }).catch((err) => {
+        console.log(err);
+    });
+    cc.say("#botbear1110", `Left channel ${user}. Reason: Banned/deleted channel`)
+})
 }
-    
+
+let namechange = await tools.nameChanges();
+
+if (await namechange.length) {
+_.each(namechange, async function (name) {
+    cc.join(name[0]).then((data) => {
+        // data returns [channel]
+    }).catch((err) => {
+        console.log(err);
+    });
+
+    cc.part(name[1]).then((data) => {
+        // data returns [channel]
+    }).catch((err) => {
+        console.log(err);
+    });
+
+    cc.say(`#${name[0]}`, `Name change detected, ${name[1]} -> ${name[0]}`)
+    cc.say("#botbear1110", `Left channel ${name[1]}. Reason: Name change detected, ${name[1]} -> ${name[0]}`)
+})
+}
+
+}
 module.exports = { cc , uptime};
