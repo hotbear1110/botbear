c16df39622623565846fd7844891cd5c7bf43d41
hotbear1110 <mads095h@gmail.com>
2021-10-09 16:07:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,6 +24,41 @@ let uptime = new Date().getTime();
 const talkedRecently = new Set();
 let oldmessage = "";
 
+await tools.refreshCommands();
+
+let bannedUsers = await tools.bannedStreamer;
+
+if (await bannedUsers.length) {
+    _.each(bannedUsers, async function (user) {
+        cc.part(user).then((data) => {
+            // data returns [channel]
+        }).catch((err) => {
+            console.log(err);
+        });
+        cc.say("#botbear1110", `Left channel ${user}. Reason: Banned/deleted channel`)
+    })
+}
+
+let namechange = await tools.nameChanges;
+
+if (await namechange.length) {
+    _.each(namechange, async function (name) {
+        cc.join(name[0]).then((data) => {
+            // data returns [channel]
+        }).catch((err) => {
+            console.log(err);
+        });
+
+        cc.part(name[1]).then((data) => {
+            // data returns [channel]
+        }).catch((err) => {
+            console.log(err);
+        });
+
+        cc.say(`#${name[0]}`, `Name change detected, ${name[1]} -> ${name[0]}`)
+        cc.say("#botbear1110", `Left channel ${name[1]}. Reason: Name change detected, ${name[1]} -> ${name[0]}`)
+    })
+}
 
 async function onMessageHandler(channel, user, msg, self) {
     let start = new Date().getTime();
@@ -199,41 +234,5 @@ async function onMessageHandler(channel, user, msg, self) {
 
 async function onConnectedHandler(addr, port) {
     console.log(`* Connected to ${addr}:${port}`);
-
-    await tools.refreshCommands();
-
-    let bannedUsers = await tools.bannedStreamer;
-
-    if (await bannedUsers.length) {
-        _.each(bannedUsers, async function (user) {
-            cc.part(user).then((data) => {
-                // data returns [channel]
-            }).catch((err) => {
-                console.log(err);
-            });
-            cc.say("#botbear1110", `Left channel ${user}. Reason: Banned/deleted channel`)
-        })
-    }
-
-    let namechange = await tools.nameChanges;
-
-    if (await namechange.length) {
-        _.each(namechange, async function (name) {
-            cc.join(name[0]).then((data) => {
-                // data returns [channel]
-            }).catch((err) => {
-                console.log(err);
-            });
-    
-            cc.part(name[1]).then((data) => {
-                // data returns [channel]
-            }).catch((err) => {
-                console.log(err);
-            });
-    
-            cc.say(`#${name[0]}`, `Name change detected, ${name[1]} -> ${name[0]}`)
-            cc.say("#botbear1110", `Left channel ${name[1]}. Reason: Name change detected, ${name[1]} -> ${name[0]}`)
-        })
-    }
 }
 module.exports = { cc , uptime};
