ce613fab7106f2cc8677945d51e4e9d8a9e4ef67
hotbear1110 <mads095h@gmail.com>
2021-10-15 14:59:13 +0200

    Compensating for rate limits

--- loops/loops.js
+++ loops/loops.js
@@ -11,7 +11,7 @@ setInterval(async function () {
     const myping = await tools.query(`SELECT * FROM MyPing`);
 
     _.each(streamers, async function (stream) {
-        setTimeout(function () { }, 200);
+        setTimeout(async function () { 
         await axios.get(`https://api.twitch.tv/helix/streams?user_login=${stream.username}`, {
             headers: {
                 'client-id': process.env.TWITCH_CLIENTID,
@@ -57,9 +57,10 @@ setInterval(async function () {
             .then(function () {
                 // always executed
             });
+        }, 500);
     })
     _.each(streamers, async function (stream) {
-        setTimeout(function () { }, 200);
+        setTimeout(async function () { 
         await axios.get(`https://api.twitch.tv/helix/channels?broadcaster_id=${stream.uid}`, {
             headers: {
                 'client-id': process.env.TWITCH_CLIENTID,
@@ -134,8 +135,9 @@ setInterval(async function () {
             .then(function () {
                 // always executed
             });
+        }, 500);
     })
-}, 10000);
+}, 20000);
 
 setInterval(async function () {
     const streamers = await tools.query('SELECT * FROM Streamers');
