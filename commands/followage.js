8bec7c1d4608cbca250d4469ec8b1de1dc495d8b
hotbear1110 <mads095h@gmail.com>
2021-10-22 00:42:24 +0200

    Update followage.js

--- commands/followage.js
+++ commands/followage.js
@@ -15,15 +15,16 @@ module.exports = {
                 return;
             }
             let username = user.username;
+            let realchannel = channel;
             if (input[2]) {
                 if (input[2].startsWith("@")) {
                     input[2] = input[2].substring(1);
                 }
+                if (input[3]) {
+                    realchannel = input[3];
+                }
+    
                 username = input[2];
-            }
-            let realchannel = channel;
-            if (input[3]) {
-                realchannel = input[3];
             }
 
             const followcheck = await axios.get(`https://api.ivr.fi/twitch/subage/${username}/${realchannel}`, {timeout: 10000});
