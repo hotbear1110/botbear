fc582c999c911c516481ab35d6ff0d76dda30e6f
TomNordesen <joachimflottorp@gmail.com>
2021-10-28 17:27:45 +0200

    Nice Commit! :)

--- tools/tools.js
+++ tools/tools.js
@@ -109,22 +109,10 @@ exports.Cooldown = class Cooldown {
     }
 
     /**
-     * @returns {String}: MM:SS
+     * @returns {String}: String formatted time left.
      */
     formattedTime() {
-        try {
-            const timeInSeconds = (this.cooldown[this.key] - process.uptime());
-            const sec = parseInt(timeInSeconds, 10); // convert value to number if it's string
-            let hours   = Math.floor(sec / 3600); // get hours
-            let minutes = Math.floor((sec - (hours * 3600)) / 60); // get minutes
-            let seconds = sec - (hours * 3600) - (minutes * 60); //  get seconds
-            // add 0 if value < 10; Example: 2 => 02
-            if (minutes < 10) {minutes = "0"+minutes;}
-            if (seconds < 10) {seconds = "0"+seconds;}
-            return minutes+':'+seconds;
-        } catch (err) {
-            console.error(err)
-        }
+        return exports.humanizeDuration(this.cooldown[this.key] - process.uptime());
     }
 };
 
