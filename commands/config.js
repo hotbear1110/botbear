46f959e7f08637446336398617fddaf5479be020
TomNordesen <joachimflottorp@gmail.com>
2021-11-16 18:09:44 +0100

    Nice Commit! :)

---
+++ commands/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: "NAME",
+    ping: true,
+    description: 'DESCRIPTION',
+    permission: 100,
+    category: "Dev command",
+    execute: async (channel, user, input, perm) => {
+        try {
+            if (module.exports.permission > perm) {
+                return;
+            }
+            return "THIS";
+        } catch (err) {
+            console.log(err);
+            return `FeelsDankMan Error`;
+        }
+    }
+}
