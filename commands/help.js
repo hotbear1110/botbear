b11aa960acd8380045d483af459d631bcdeb73bf
hotbear1110 <mads095h@gmail.com>
2022-01-16 22:13:09 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -14,8 +14,13 @@ module.exports = {
             if (!input[2]) {
                 return 'List of commands: https://hotbear.xyz/ - If you want help with a command, write: "bb help *command*"';
             }
-            const Alias = new tools.Alias(`bb ${input[2]}`);
-            realcommand = input[2].replace(Alias.getRegex(), Alias.getReplacement()).split(' ');
+
+            let aliasList = await tools.query(`SELECT Aliases FROM Aliases`);
+
+            aliasList = JSON.parse(aliasList[0].Aliases);
+
+            const Alias = new tools.Alias(`bb ${input[2]}`, aliasList);
+            let realcommand = input[2].replace(Alias.getRegex(), Alias.getReplacement()).split(' ');
             const commandlist = await tools.query(`SELECT * FROM Commands WHERE Name=?`, [realcommand]);
 
             if (!commandlist.length) {
