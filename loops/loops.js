86d70d6c87aef104d04692cfb92d9db52658d68f
hotbear1110 <mads095h@gmail.com>
2022-02-05 14:38:56 +0100

    Testing faster rate limit

--- loops/loops.js
+++ loops/loops.js
@@ -152,7 +152,7 @@ setInterval(async function () {
         }, 500);
     }
     )
-}, 20000);
+}, 5000);
 
 setInterval(async function () {
     const streamers = await tools.query('SELECT * FROM Streamers');
