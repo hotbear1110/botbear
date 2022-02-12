753792d961606e05dae04c09ffe6472330e705f7
hotbear1110 <mads095h@gmail.com>
2022-02-12 15:46:22 +0100

    Added a giveflower command

---
+++ commands/giveflower.js
@@ -0,0 +1,21 @@
+module.exports = {
+    name: "giveflower",
+    ping: false,
+    description: 'Give a chatter a flower nymnHappy',
+    permission: 100,
+    category: "Random command",
+    execute: async (channel, user, input, perm) => {
+        try {
+            if (module.exports.permission > perm) {
+                return;
+            }
+            if (input[2]) {
+                return `${input[2]}, nymnFlower`;
+            }
+            return `${user.username}, nymnFlower`;
+        } catch (err) {
+            console.log(err);
+            return `FeelsDankMan Error`;
+        }
+    }
+}
