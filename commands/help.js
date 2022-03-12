5d5a8baf29647126939e4011881c8f094da96344
SotirisVas <sotiris01@hotmail.gr>
2022-03-12 12:50:26 +0200

    Added CD for trivia help

--- commands/help.js
+++ commands/help.js
@@ -12,9 +12,8 @@ module.exports = {
                 return;
             }
             if (!input[2]) {
-                return 'List of commands: https://hotbear.org/ - If you want help with a command, write: "bb help *command*"';
+                return 'List of commands: https://hotbear.xyz/ - If you want help with a command, write: "bb help *command*"';
             }
-
             let aliasList = await tools.query(`SELECT Aliases FROM Aliases`);
 
             aliasList = JSON.parse(aliasList[0].Aliases);
@@ -24,13 +23,32 @@ module.exports = {
             const commandlist = await tools.query(`SELECT * FROM Commands WHERE Name=?`, [realcommand.toLowerCase()]);
 
             if (!commandlist.length) {
-                return;
+                return 'Command not found FeelsDankMan';
             }
 
-            return `${commandlist[0].Command} - Permission lvl: ${commandlist[0].Perm}`;
+            if (commandlist[0].Name === 'trivia' || commandlist[0].Name === 'trivia2') {
+                let realchannel = channel.substring(1);
+                let cd = await tools.query("SELECT `trivia_cooldowns` FROM `Streamers` WHERE `username` = ?", [realchannel]);
+        
+                if (cd[0].trivia_cooldowns === null) {
+                    cd[0].trivia_cooldowns === 30000;
+                    tools.query("UPDATE `Streamers` SET `trivia_cooldowns` = 30000 WHERE `username` = ?", [realchannel]);
+                } 
+        
+                if(commandlist[0].Name === 'trivia'){
+                    return `This command will start a new trivia in chat (The cooldown is at ${cd[0].trivia_cooldowns.formattedTime()} and the trivia times out after 60 seconds.).
+                                Specific category: "bb trivia sports". Categories: [ https://haste.zneix.eu/uhedagatig.txt ] - Permission lvl: ${commandlist[0].Perm}`;
+                } else {
+                    return `This command will start a new trivia in chat (The cooldown is at ${cd[0].trivia_cooldowns.formattedTime()} and the trivia times out after 60 seconds.)
+                                 - Api used: [ https://gazatu.xyz/ ] - Permission lvl: ${commandlist[0].Perm}`;
+                }      
+            } else {
+                return `${commandlist[0].Command} - Permission lvl: ${commandlist[0].Perm}`; 
+            }         
         } catch (err) {
             console.log(err);
             return `FeelsDankMan Sql error: ${err.sqlMessage}`;
+            
         }
     }
 }
