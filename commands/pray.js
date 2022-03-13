568c841af537aa80b8f949e178be3592c28787d6
Arthur <oxenarthur@gmail.com>
2022-03-13 23:37:30 +0200

    added random quote

---
+++ commands/pray.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const got = require('got');
+
+module.exports = {
+    name: "pray",
+    ping: true,
+    description: 'This command will give a random bible quote',
+    permission: 100,
+    category: "Random command",
+    execute: async (channel, user, input, perm) => {
+        try {
+            if (module.exports.permission > perm) {
+                return;
+            }
+
+            const url = 'https://labs.bible.org/api/?passage=random&type=json';
+            
+            const response = await got.get(url).json();
+            return `[${response.bookname} ${response.chapter}:${response.verse}]: ${response.text} Prayge`;
+
+        } catch (err) {
+            console.log(err);
+            return `FeelsDankMan lost my bible`;
+        }
+    }
+}
