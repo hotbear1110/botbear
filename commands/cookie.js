3eba50798e81178ca0690a65683a88be2fc56faa
hotbear1110 <mads095h@gmail.com>
2022-03-18 20:32:36 +0100

    added info to cdr and cookie register

--- commands/cookie.js
+++ commands/cookie.js
@@ -19,7 +19,7 @@ module.exports = {
                     if (!register.length) {
                         await tools.query('INSERT INTO Cookies (User) values (?)', [user.username]);
 
-                        return 'You are now registered for cookie notifications';
+                        return 'You are now registered for cookie notifications. Do "bb cookie mode" to change where the bot responds';
 
                     } else {
                         return 'You are already registered for cookie notifications';
