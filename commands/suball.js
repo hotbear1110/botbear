50b302cafc679fa0f1619648ad2b65e1fc3e2c62
hotbear1110 <mads095h@gmail.com>
2022-03-30 19:49:22 +0200

    Update suball.js

--- commands/suball.js
+++ commands/suball.js
@@ -5,7 +5,7 @@ const tools = require("../tools/tools.js");
 module.exports = {
     name: "suball",
     ping: false,
-    description: 'This command is for subbing to channel.update for all streamers in db',
+    description: 'This command is for subbing to channel.update, channel.online and channel.offline for all streamers in db',
     permission: 2000,
     category: "Dev command",
     execute: async (channel, user, input, perm) => {
@@ -24,6 +24,46 @@ module.exports = {
 
                     let data = JSON.stringify({
                         "type": "channel.update",
+                        "version": "1",
+                        "condition": { "broadcaster_user_id": uid.toString() },
+                        "transport": { "method": "webhook", "callback": "https://hotbear.org/eventsub", "secret": process.env.TWITCH_SECRET }
+                    });
+                    console.log(uid)
+                    await got.post(`https://api.twitch.tv/helix/eventsub/subscriptions`, {
+                        headers: {
+                            'client-id': process.env.TWITCH_CLIENTID,
+                            'Authorization': process.env.TWITCH_AUTH,
+                            'Content-Type': 'application/json'
+                        },
+                        body: data
+                    });
+                }, 100 * i)
+
+                setTimeout(async function () {
+                    let uid = streamers[i].uid;
+
+                    let data = JSON.stringify({
+                        "type": "channel.online",
+                        "version": "1",
+                        "condition": { "broadcaster_user_id": uid.toString() },
+                        "transport": { "method": "webhook", "callback": "https://hotbear.org/eventsub", "secret": process.env.TWITCH_SECRET }
+                    });
+                    console.log(uid)
+                    await got.post(`https://api.twitch.tv/helix/eventsub/subscriptions`, {
+                        headers: {
+                            'client-id': process.env.TWITCH_CLIENTID,
+                            'Authorization': process.env.TWITCH_AUTH,
+                            'Content-Type': 'application/json'
+                        },
+                        body: data
+                    });
+                }, 100 * i)
+
+                setTimeout(async function () {
+                    let uid = streamers[i].uid;
+
+                    let data = JSON.stringify({
+                        "type": "channel.offline",
                         "version": "1",
                         "condition": { "broadcaster_user_id": uid.toString() },
                         "transport": { "method": "webhook", "callback": "https://hotbear.org/eventsub", "secret": process.env.TWITCH_SECRET }
