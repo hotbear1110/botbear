d5f3ebf2400901b551c54da7c1d4a8a487bbf3f2
hotbear1110 <mads095h@gmail.com>
2022-04-11 21:20:42 +0200

    Added randommod/vip commands

---
+++ commands/randommod.js
@@ -0,0 +1,38 @@
+const got = require("got");
+const _ = require("underscore");
+const tools = require("../tools/tools.js");
+
+
+module.exports = {
+    name: "randommod",
+    ping: true,
+    description: 'This command will give you the name if a random mod in a given channel',
+    permission: 100,
+    category: "Info command",
+    execute: async (channel, user, input, perm) => {
+        try {
+            if (module.exports.permission > perm) {
+                return;
+            }
+            let realchannel = channel;
+            if (input[2]) {
+                realchannel = input[2];
+            }
+            let modcheck = await got(`https://api.ivr.fi/twitch/modsvips/${realchannel}`, { timeout: 10000 }).json();
+            let mods = modcheck["mods"];
+            let number = Math.floor(Math.random() * (mods.length - 0) + 0);
+
+            let ms = new Date().getTime() - Date.parse(mods[number].grantedAt)
+            return `Random M OMEGALUL D in #${realchannel[0]}\u{E0000}${realchannel.slice(1)} is ${mods[number].displayName}, they were added ${tools.humanizeDuration(ms)} ago.`;
+
+        } catch (err) {
+            console.log(err);
+            if (err.name) {
+                if (err.name === "TimeoutError") {
+                    return `FeelsDankMan api error: ${err.name}`;
+                }
+            }
+            return `FeelsDankMan Error`;
+        }
+    }
+}
