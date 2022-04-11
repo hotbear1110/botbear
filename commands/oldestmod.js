570c03d25db998ad495160d898ef5828b8cf8b9c
hotbear1110 <mads095h@gmail.com>
2022-04-11 20:57:46 +0200

    made oldestvip/mod commands

--- commands/oldestmod.js
+++ commands/oldestmod.js
@@ -4,9 +4,9 @@ const tools = require("../tools/tools.js");
 
 
 module.exports = {
-    name: "latestvip",
+    name: "oldestmod",
     ping: true,
-    description: 'This command will give you the name if the newest vip in a given channel',
+    description: 'This command will give you the name if the oldest mod in a given channel',
     permission: 100,
     category: "Info command",
     execute: async (channel, user, input, perm) => {
@@ -18,11 +18,11 @@ module.exports = {
             if (input[2]) {
                 realchannel = input[2];
             }
-            let vipcheck = await got(`https://api.ivr.fi/twitch/modsvips/${realchannel}`, { timeout: 10000 }).json();
-            let vips = vipcheck["vips"];
+            let modcheck = await got(`https://api.ivr.fi/twitch/modsvips/${realchannel}`, { timeout: 10000 }).json();
+            let mods = modcheck["mods"];
 
-            let ms = new Date().getTime() - Date.parse(vips[vips.length - 1].grantedAt)
-            return `The newest vip😬 in #${realchannel[0]}\u{E0000}${realchannel.slice(1)} is ${vips[vips.length - 1].displayName}, they were added ${tools.humanizeDuration(ms)} ago.`;
+            let ms = new Date().getTime() - Date.parse(mods[0].grantedAt)
+            return `The oldest M OMEGALUL D in #${realchannel[0]}\u{E0000}${realchannel.slice(1)} is ${mods[0].displayName}, they were added ${tools.humanizeDuration(ms)} ago.`;
 
         } catch (err) {
             console.log(err);
--- commands/oldestvip.js
+++ commands/oldestvip.js
@@ -4,9 +4,9 @@ const tools = require("../tools/tools.js");
 
 
 module.exports = {
-    name: "latestvip",
+    name: "oldestvip",
     ping: true,
-    description: 'This command will give you the name if the newest vip in a given channel',
+    description: 'This command will give you the name if the oldest vip in a given channel',
     permission: 100,
     category: "Info command",
     execute: async (channel, user, input, perm) => {
@@ -21,8 +21,8 @@ module.exports = {
             let vipcheck = await got(`https://api.ivr.fi/twitch/modsvips/${realchannel}`, { timeout: 10000 }).json();
             let vips = vipcheck["vips"];
 
-            let ms = new Date().getTime() - Date.parse(vips[vips.length - 1].grantedAt)
-            return `The newest vip😬 in #${realchannel[0]}\u{E0000}${realchannel.slice(1)} is ${vips[vips.length - 1].displayName}, they were added ${tools.humanizeDuration(ms)} ago.`;
+            let ms = new Date().getTime() - Date.parse(vips[0].grantedAt)
+            return `The newest vip😬 in #${realchannel[0]}\u{E0000}${realchannel.slice(1)} is ${vips[0].displayName}, they were added ${tools.humanizeDuration(ms)} ago.`;
 
         } catch (err) {
             console.log(err);
