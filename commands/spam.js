fb3a2cf2fdc99e7358111d84ba6308b41e3eadaa
hotbear1110 <mads095h@gmail.com>
2022-04-23 18:09:00 +0200

    Added a spam command for devs and fixed % in botstats repsonse

---
+++ commands/spam.js
@@ -0,0 +1,28 @@
+const cc = require("../bot.js").cc;
+
+module.exports = {
+    name: "spam",
+    ping: true,
+    description: 'spam something',
+    permission: 100,
+    category: "Dev command",
+    execute: async (channel, user, input, perm) => {
+        try {
+            if (module.exports.permission > perm) {
+                return;
+            }
+            let message = input.toString();
+            message.shift()
+            message.shift()
+
+            for (let i = 0; i < input[2]; i++) {
+                cc.say(channel, message);
+            }
+
+            return;
+        } catch (err) {
+            console.log(err);
+            return `FeelsDankMan Error`;
+        }
+    }
+}
