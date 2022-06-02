19fa61c70b13fd20bd8c337f3ac513b9ab6af5e6
Mads Hauberg <mads095h@gmail.com>
2022-06-02 17:33:48 +0200

    Update fl.js

--- commands/fl.js
+++ commands/fl.js
@@ -43,7 +43,7 @@ module.exports = {
                 }
                     return true
                 }
-            fl = fl[0] ? fl[0] : f;
+            fl = fl[0] ? fl[0] : fl;
 console.log(fl)
             let realfl = fl[0];
 
