7db1f82fff6518e120f625b0ba2631e99255c9a8
Mads Hauberg <mads095h@gmail.com>
2022-06-14 16:41:07 +0200

    Update follows.js

--- commands/follows.js
+++ commands/follows.js
@@ -16,7 +16,12 @@ module.exports = {
 
             if(input[2]) {
                 try {
-                const getuid = await got(`https://api.ivr.fi/v2/twitch/user/${input[2]}`, { timeout: 10000 }).json();
+                const getuid = await got(`https://api.ivr.fi/v2/twitch/user/${input[2]}`, { headers: {
+                    'client-id': process.env.TWITCH_CLIENTID,
+                    'Authorization': process.env.TWITCH_AUTH
+                },
+                timeout: 10000 
+            }).json();
                 realuid = getuid.data[0].id;
                 realuser = input[2];
                 }
