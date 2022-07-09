e74690322c3fbc23cf049a93c3ee066bcebdf55b
Mads Hauberg <mads095h@gmail.com>
2022-07-09 18:33:14 +0200

    Improved lm/ll
    Lm/ll now excludes sub messages/bans/timeouts and can also look at multple months

--- commands/lm.js
+++ commands/lm.js
@@ -1,6 +1,6 @@
 const got = require("got");
+const { each } = require("underscore");
 const tools = require("../tools/tools.js");
-
 
 module.exports = {
     name: "lm",
@@ -14,12 +14,14 @@ module.exports = {
                 return;
             }
             let uid = user["user-id"];
+            let realuser = user.username;
             if (input[2]) {
                 if (input[2].startsWith("@")) {
                     input[2] = input[2].substring(1);
                 }
                 uid = await got(`https://api.ivr.fi/twitch/resolve/${input[2]}`, { timeout: 10000 }).json();
                 uid = uid.id;
+                realuser = input[2]
             }
             let realchannel = channel;
             if (input[3]) {
@@ -29,35 +31,49 @@ module.exports = {
             if (channel === realchannel && user["user-id"] === uid) {
                 return "nymnDank But you are right here ❓❗"
             }
+            let logDate = await got(`https://logs.ivr.fi/list?channel=${realchannel}&userid=${uid}`, { timeout: 10000 }).json();
 
-            const lm = await got(`https://logs.ivr.fi/channel/${realchannel}/userid/${uid}?json&reverse`, { timeout: 10000 }).json();
+            logDate = logDate.availableLogs;
 
-            function filterByID(message) {
-                if (message.type !== 1) {
-                    return false
+            let messageFound = false;
+            let i = 0;
+            let realmessages = "";
+            let lm = "";
+            while (!messageFound) {
+                let year = logDate[i].year;
+                let month = logDate[i].month;
+
+                lm = await got(`https://logs.ivr.fi/channel/${realchannel}/userid/${uid}/${year}/${month}?json`, { timeout: 10000 }).json();
+
+                function filterByID(message) {
+                    if (message.type !== 1) {
+                        return false
+                    }
+                    return true
                 }
-                return true
+
+                realmessages = lm.messages.filter(filterByID);
+
+                if (realmessages[0]) {
+                    messageFound = true;
+                    realmessages = realmessages[0];
+                }
+                i++
+                if (i >= logDate.length) {
+                    return `FeelsDankMan @${realuser}, has never said anything in #${realchannel}`;
+                }
             }
-            let messages = lm.messages;
 
-            let realmessages = messages;
+            console.log(realmessages)
+            let message = tools.splitLine(realmessages.text, 350)
 
-            realmessages = messages.filter(filterByID);
-
-
-            if (!realmessages[0]) {
-                realmessages = messages;
-            }
-
-            let message = tools.splitLine(realmessages[0].text, 350)
-
-            const timeago = new Date().getTime() - Date.parse(realmessages[0].timestamp);
+            const timeago = new Date().getTime() - Date.parse(realmessages.timestamp);
 
             if (lm.status !== 404) {
                 if (message[1]) {
                     return `#${realchannel} ${realmessages[0].displayName}: ${message[0]}... - (${tools.humanizeDuration(timeago)} ago)`;
                 }
-                return `nymnDank ${realmessages[0].displayName}'s last message in #${realchannel[0]}\u{E0000}${realchannel.slice(1)} was: ${message} - (${tools.humanizeDuration(timeago)} ago)`;
+                return `nymnDank ${realmessages.displayName}'s last message in #${realchannel[0]}\u{E0000}${realchannel.slice(1)} was: ${message} - (${tools.humanizeDuration(timeago)} ago)`;
             }
 
         } catch (err) {
