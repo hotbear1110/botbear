d4e4108e442dd70d9b0abd89fa6d176c11bdeb2f
Mads Hauberg <mads095h@gmail.com>
2022-07-12 21:32:49 +0200

    Cleaned up command index

--- commands/index.js
+++ commands/index.js
@@ -28,50 +28,45 @@ module.exports = {
 
         // TODO Melon: This could look better.
         for (const command of commands) {
-            const UpdateDatabase = async () => {
-                for (const dbcommand of dbCommands) {
-                    if (dbcommand.Name === command.name) {
-                        if (
-                            dbcommand.Command !== command.description ||
-                            dbcommand.Perm !== command.permission ||
-                            dbcommand.Category !== command.category ||
-                            dbcommand.Cooldown !== command.cooldown
-                        ) {
-                            sql.Query(`UPDATE Commands SET Command=?, Perm=?, Category=?, Cooldown=? WHERE Name=?`, [command.description, command.permission, command.category, command.cooldown, command.name]);
-                        }
-                    } else {
-                        return false;
+            for (const dbcommand of dbCommands) {
+                if (dbcommand.Name === command.name) {
+                    if (
+                        dbcommand.Command !== command.description ||
+                        dbcommand.Perm !== command.permission ||
+                        dbcommand.Category !== command.category ||
+                        dbcommand.Cooldown !== command.cooldown
+                    ) {
+                        sql.Query(`UPDATE Commands SET Command=?, Perm=?, Category=?, Cooldown=? WHERE Name=?`, [command.description, command.permission, command.category, command.cooldown, command.name]);
                     }
                 }
-                return true;
-            }
-
-            if (await UpdateDatabase()) {
-                if (disableCommand.length && command.category !== "Core command" && command.category !== "Dev command") {
-                    for (const user of disableCommand) {
-                        let disabledList = await sql.Query(`
-                        SELECT disabled_commands
-                        FROM Streamers
-                        WHERE username=?`,
-                            [user.username]);
-
-                        console.log(disabledList)
-                        disabledList = JSON.parse(disabledList[0].disabled_commands);
-                        disabledList.push(command.name);
-                        disabledList = JSON.stringify(disabledList);
-
-                        await sql.Query(`UPDATE Streamers SET disabled_commands=? WHERE username=?`, [disabledList, user.username]);
-
-                    }
-                }
-                await sql.Query('INSERT INTO Commands (Name, Command, Perm, Category, Cooldown) values (?, ?, ?, ?, ?)', [command.name, command.description, command.permission, command.category, command.cooldown]);
             }
         }
-        const commandNames = commands.map(x => x.name);
-        console.log(dbCommands.map(x => x.name))
-        const commandDiff = dbCommands.filter(x => !commandNames.includes(x.Name));
 
-        for (const dbCommand of commandDiff) {
+        const commandNames = commands.map(x => x.name);
+        const dbcommandNames = dbCommands.map(x => x.Name);
+        const commandDiff = commands.filter(x => !dbcommandNames.includes(x.name));
+        const dbcommandDiff = dbCommands.filter(x => !commandNames.includes(x.Name));
+        for (const command of commandDiff) {
+            if (disableCommand.length && command.category !== "Core command" && command.category !== "Dev command") {
+                for (const user of disableCommand) {
+                    let disabledList = await sql.Query(`
+                    SELECT disabled_commands
+                    FROM Streamers
+                    WHERE username=?`,
+                        [user.username]);
+
+                    disabledList = JSON.parse(disabledList[0].disabled_commands);
+                    disabledList.push(command.name);
+                    disabledList = JSON.stringify(disabledList);
+
+                    await sql.Query(`UPDATE Streamers SET disabled_commands=? WHERE username=?`, [disabledList, user.username]);
+
+                }
+            }
+            await sql.Query('INSERT INTO Commands (Name, Command, Perm, Category, Cooldown) values (?, ?, ?, ?, ?)', [command.name, command.description, command.permission, command.category, command.cooldown]);
+        }
+
+        for (const dbCommand of dbcommandDiff) {
             await sql.Query('DELETE FROM Commands WHERE Name=?', [dbCommand.Name]);
         }
 
