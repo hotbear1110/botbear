cc7b12c2854cd837719695d73b88868e1d10c58f
Mads Hauberg <mads095h@gmail.com>
2022-08-01 18:34:19 +0200

    Update app.js

--- web/app.js
+++ web/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const favicon = require('express-favicon');
 var http = require('http');
+var fs = require('fs');
+var https = require('https');
+var privateKey = fs.readFileSync('/etc/letsencrypt/live/hotbear.xyz/privkey.pem');
+var certificate = fs.readFileSync('/etc/letsencrypt/live/hotbear.xyz/fullchain.pem');
+
+var credentials = { key: privateKey, cert: certificate };
 
 
 const app = express();
@@ -22,5 +28,7 @@ app.use('/', commandRouter);
 app.use('/suggestions', suggestionRouter);
 
 var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
 
-httpServer.listen(4000);
+httpServer.listen(3000);
+httpsServer.listen(4000);
