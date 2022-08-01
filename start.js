cccfa21a079c69a666f430cbfdec5e3428a676e3
Mads Hauberg <mads095h@gmail.com>
2022-08-01 16:51:02 +0200

    Removed ssl stuff from /web

--- start.js
+++ start.js
@@ -37,7 +37,6 @@ init().then(() => {
 	//require("./tools/logger.js");
 	require('./bot.js');
 	require('./loops/loops.js');
-	require('./tools/webhook.js');
 	console.log('Ready!');
 })
 	.catch((e) => {
