03a5d9f05e35d751bd93d2e62ed711fc02f1f60f
Mads Hauberg <mads095h@gmail.com>
2022-08-17 17:48:13 +0200

    Create apexmap.js
    Made by: @sougataghar477
    
    Co-Authored-By: sougataghar477 <50982886+sougataghar477@users.noreply.github.com>

---
+++ commands/apexmap.js
@@ -0,0 +1,37 @@
+const got = require('got');
+require('dotenv').config();
+//Made by: @sougataghar477
+module.exports = {
+  name: 'apexmap',
+  ping: true,
+  description: 'This command will give you the current map in apex pubs',
+  permission: 100,
+  category: 'Random command',
+  execute: async (channel, user, input, perm) => {
+    try {
+      if (module.exports.permission > perm) {
+        return;
+      }
+      const map = await got(
+        `https://api.mozambiquehe.re/maprotation?version=2&auth=${process.env.APEX_API}`,
+        { 
+            timeout: 10000 
+        }
+      ).json();
+
+      let { map: currentMap, remainingTimer } = map.battle_royale.current;
+
+      let { map: nextMap, DurationInMinutes } = map.battle_royale.next;
+
+      return `Current map is ${currentMap} which lasts for ${remainingTimer} .Next map is ${nextMap} which lasts for ${DurationInMinutes} minutes.`;
+    } catch (err) {
+      console.log(err);
+      if (err.name) {
+        if (err.name === 'TimeoutError') {
+          return `FeelsDankMan api error: ${err.name}`;
+        }
+      }
+      return 'FeelsDankMan Error';
+    }
+  },
+};
