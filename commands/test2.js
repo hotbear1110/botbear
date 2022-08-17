8662f6f4cff3066daa358079899b411befdba98f
Mads Hauberg <mads095h@gmail.com>
2022-08-17 15:55:17 +0200

    yes

--- commands/test2.js
+++ commands/test2.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const sql = require('./../sql/index.js');
+const got = require('got');
 
 module.exports = {
 	name: 'test2',
@@ -12,13 +13,92 @@ module.exports = {
 			if (module.exports.permission > perm) {
 				return;
 			}
-			let [bigtest] = await sql.Query(`SELECT RemindTime, Status, User
-                        FROM Cdr 
-                        WHERE User = ? 
-                            OR User = ?
-                            OR User = ?`, 
-        [input[3], input[2], input[1].replace(',', '')]);
-		console.log(bigtest);
+			let allsubs = [];
+			let haspagnation = true;
+			let pagnation = '';
+			while (haspagnation) {
+				let subs = await got(`https://api.twitch.tv/helix/eventsub/subscriptions?after=${pagnation}`, {
+					headers: {
+						'client-id': process.env.TWITCH_CLIENTID,
+						'Authorization': process.env.TWITCH_AUTH
+					}
+				});
+				subs = JSON.parse(subs.body);
+				if (subs.pagination.cursor) {
+					pagnation = subs.pagination.cursor;
+				} else {
+					haspagnation = false;
+				}
+				subs = subs.data.id;
+				allsubs = allsubs.concat(subs);
+			}
+
+			const streamers = await sql.Query(`
+            SELECT uid
+            FROM Streamers`);
+
+			for (let i = 0; i < streamers.length; i++) {
+				if (!allsubs.includes(streamers[i].uid))
+				setTimeout(async function () {
+					let uid = streamers[i].uid;
+
+					let data = JSON.stringify({
+						'type': 'channel.update',
+						'version': '1',
+						'condition': { 'broadcaster_user_id': uid.toString() },
+						'transport': { 'method': 'webhook', 'callback': 'https://hotbear.org/eventsub', 'secret': process.env.TWITCH_SECRET }
+					});
+					console.log(uid);
+					await got.post('https://api.twitch.tv/helix/eventsub/subscriptions', {
+						headers: {
+							'client-id': process.env.TWITCH_CLIENTID,
+							'Authorization': process.env.TWITCH_AUTH,
+							'Content-Type': 'application/json'
+						},
+						body: data
+					});
+				}, 100 * i);
+
+				setTimeout(async function () {
+					let uid = streamers[i].uid;
+
+					let data = JSON.stringify({
+						'type': 'stream.online',
+						'version': '1',
+						'condition': { 'broadcaster_user_id': uid.toString() },
+						'transport': { 'method': 'webhook', 'callback': 'https://hotbear.org/eventsub', 'secret': process.env.TWITCH_SECRET }
+					});
+					console.log(uid);
+					await got.post('https://api.twitch.tv/helix/eventsub/subscriptions', {
+						headers: {
+							'client-id': process.env.TWITCH_CLIENTID,
+							'Authorization': process.env.TWITCH_AUTH,
+							'Content-Type': 'application/json'
+						},
+						body: data
+					});
+				}, 100 * i);
+
+				setTimeout(async function () {
+					let uid = streamers[i].uid;
+
+					let data = JSON.stringify({
+						'type': 'stream.offline',
+						'version': '1',
+						'condition': { 'broadcaster_user_id': uid.toString() },
+						'transport': { 'method': 'webhook', 'callback': 'https://hotbear.org/eventsub', 'secret': process.env.TWITCH_SECRET }
+					});
+					console.log(uid);
+					await got.post('https://api.twitch.tv/helix/eventsub/subscriptions', {
+						headers: {
+							'client-id': process.env.TWITCH_CLIENTID,
+							'Authorization': process.env.TWITCH_AUTH,
+							'Content-Type': 'application/json'
+						},
+						body: data
+					});
+				}, 100 * i);
+			}
 			return 'Okayge done';
 		} catch (err) {
 			console.log(err);
