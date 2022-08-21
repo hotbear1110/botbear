f40d3ac1fc32620288f8fb86adccd06d403ba3d1
Mads Hauberg <77441913+hotbear1110@users.noreply.github.com>
2022-08-21 17:27:25 +0200

    Added some "security", so the bot can't do commands

--- commands/yoda.js
+++ commands/yoda.js
@@ -1,12 +1,13 @@
 const got = require('got');
 require('dotenv').config();
 const tools = require('../tools/tools.js');
+const regex = require('../tools/regex.js');
 
 //Made by: @sougataghar477
 module.exports = {
-  name: 'apexmap',
+  name: 'yoda',
   ping: true,
-  description: 'This command will give you the current map in apex pubs',
+  description: 'This command will translate the input to Yoda speach',
   permission: 100,
   category: 'Random command',
   execute: async (channel, user, input, perm) => {
@@ -14,17 +15,36 @@ module.exports = {
       if (module.exports.permission > perm) {
         return;
       }
+      input = input.splice(2);
+      let msg = input.join(' ');
+      
+      msg.replace(regex.invisChar, '');
+      
       let yoda = await got(
-        `https://api.funtranslations.com/translate/yoda.json?text=${input.join(" ")}`,
+        `https://api.funtranslations.com/translate/yoda.json?text=${msg}`,
         { 
             timeout: 10000 
         }
       ).json();
-        // yoda = JSON.parse(yoda.body);
-
       
+      let yodaText = yoda.contents.translated;
+      
+      if (tools.isMod(user, channel) === false && perm < 2000 && msg.match(/[&|$|/|.|?|-]|\bkb\b|^\bmelon\b/g)) { // ignores &, $, kb, /, ., ?, !, - bot prefixes (. and / are twitch reserved prefixes)  
+				yodaText = yodaText.charAt(0) + '\u{E0000}' + yodaText.substring(1);
+			}
+			if (yodaText.match(/!/g)) {
+				yodaText = '❗ ' + yodaText.substring(1);
+			}
 
-      return yoda.contents.translated;
+			if (perm < 2000 && msg.match(/(\.|\/)color/g)) {
+      yodaText = '. ' + yodaText
+			}
+
+			if (msg.toLowerCase().startsWith(`/ban ${process.env.TWITCH_OWNERNAME}`) || msg.toLowerCase().startsWith(`/timeout ${process.env.TWITCH_OWNERNAME}`) || msg.toLowerCase().startsWith(`/unmod ${process.env.TWITCH_USER}`)) {
+      yodaText = '. ' + yodaText
+			}
+
+      return yodaText;
     } catch (err) {
       console.log(err);
       if (err.name) {
