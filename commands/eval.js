20eacdffe8196ca6c33413bfc3fdeda5301a8aac
Mads Hauberg <mads095h@gmail.com>
2022-09-11 15:45:08 +0200

    Made an eval command, perm 2000 for now

---
+++ commands/eval.js
@@ -0,0 +1,30 @@
+const regex = require('../tools/regex.js');
+
+module.exports = {
+	name: 'eval',
+	ping: false,
+	description: 'This command will let you execute js code in the bot and make it return the result. (The message gets checked for massping, banphrases etc.). Example: "bb eval "lole ".repeat(10);"',
+	permission: 2000,
+	category: 'Random command',
+	execute: async (channel, user, input, perm) => {
+		try {
+			if (module.exports.permission > perm) {
+				return;
+			}
+			input = input.splice(2);
+			let msg = input.join(' ');
+
+			msg.replace(regex.invisChar, '');
+
+            msg = await eval(msg).toString();
+
+			if (perm < 2000 && msg.match(/[&|$|/|.|?|-|!]|\bkb\b|^\bmelon\b/g)) { // ignores &, $, kb, /, ., ?, !, - bot prefixes (. and / are twitch reserved prefixes)  
+				msg = msg.charAt(0) + '\u{E0000}' + msg.substring(1);
+			}
+			return msg;
+		} catch (err) {
+			console.log(err);
+			return 'FeelsDankMan Error';
+		}
+	}
+};
