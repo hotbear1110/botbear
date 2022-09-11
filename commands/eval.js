8ed5de4e890141c679ed219c0c94b485d730fad2
Mads Hauberg <mads095h@gmail.com>
2022-09-11 16:17:03 +0200

    changed eval to use the vm2 package for more security
    Still permission 1500 for testing

--- commands/eval.js
+++ commands/eval.js
@@ -1,10 +1,11 @@
 const regex = require('../tools/regex.js');
+const {VM} = require('vm2');
 
 module.exports = {
 	name: 'eval',
 	ping: false,
 	description: 'This command will let you execute js code in the bot and make it return the result. (The message gets checked for massping, banphrases etc.). Example: "bb eval "lole ".repeat(10);"',
-	permission: 2000,
+	permission: 1500,
 	category: 'Random command',
 	execute: async (channel, user, input, perm) => {
 		try {
@@ -16,7 +17,7 @@ module.exports = {
 
 			msg.replace(regex.invisChar, '');
 
-            msg = await eval(msg).toString();
+            msg = await new VM().run(msg).toString();
 
 			if (perm < 2000 && msg.match(/[&|$|/|.|?|-|!]|\bkb\b|^\bmelon\b/g)) { // ignores &, $, kb, /, ., ?, !, - bot prefixes (. and / are twitch reserved prefixes)  
 				msg = msg.charAt(0) + '\u{E0000}' + msg.substring(1);
@@ -24,7 +25,7 @@ module.exports = {
 			return msg;
 		} catch (err) {
 			console.log(err);
-			return 'FeelsDankMan Error';
+			return 'FeelsDankMan ' + err.toString().split('\n')[0];
 		}
 	}
 };
