43645f52e317a7ea9e0336877a60dcce2c0a89aa
Mads Hauberg <mads095h@gmail.com>
2022-11-04 18:24:10 +0100

    bb ask was timing out

--- got/index.js
+++ got/index.js
@@ -11,7 +11,7 @@ module.exports = (() => {
                         'User-Agent': 'Botbear'
                     },
                     timeout: {
-                        request: 3500
+                        request: 5000
                     }
                 });
         }
