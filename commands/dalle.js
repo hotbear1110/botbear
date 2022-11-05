6cf023e3848610ad4b540e2af05d8742a27140f0
Mads Hauberg <mads095h@gmail.com>
2022-11-05 14:22:32 +0100

    Added dalle2 command

---
+++ commands/dalle.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+const { got } = require('./../got');
+
+module.exports = {
+	name: 'dalle',
+	ping: true,
+	description: 'Make any image you can think of using the power of dalle2 ai! - The cooldown on this is 2 min.',
+	permission: 100,
+	cooldown: 120,
+	category: 'Random command',
+	execute: async (channel, user, input, perm) => {
+		try {
+			if (module.exports.permission > perm) {
+				return;
+			}
+			if (channel !== 'nymn' && channel !== 'hottestbear' && channel !== 'elina' && channel !== 'pajlada' && !(perm >= 1500)) {
+				return 'This command is currently disabled :)';
+			}
+
+            input = input.splice(2);
+			let msg = input.join(' ');
+
+			const url = 'https://api.openai.com/v1/images/generations';
+			const params = {
+				'prompt': msg,
+				'n': 1,
+				'size': '1024x1024'
+			};
+			const headers = {
+				'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+                'Content-Type' : 'application/json'
+			};
+
+			try {
+				const response = await got.post(url, { json: params, headers: headers }).json();
+
+
+				console.log(response);
+				return `"${msg}": ` + response.data['url'];
+			} catch (err) {
+				console.log(err);
+				if (err.response.statusCode === 429) {
+					return 'Nime you have used all of this months bb ask';
+				}
+			}
+		} catch (err) {
+			console.log(err);
+			return 'FeelsDankMan Error';
+		}
+	}
+};
