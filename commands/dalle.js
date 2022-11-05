638cf3d2e578b79fd9fb37666ab76483265be47a
Mads Hauberg <mads095h@gmail.com>
2022-11-05 15:30:02 +0100

    Update dalle.js

--- commands/dalle.js
+++ commands/dalle.js
@@ -35,11 +35,12 @@ module.exports = {
 			try {
 				const response = await got.post(url, { json: params, headers: headers }).json();
 
-                const dalleImageToBlob = async (url) => Buffer.from(await got(url, { responseType: 'buffer' }).then((res) => res.body));
+                //const dalleImageToBlob = async (url) => Buffer.from(await got(url, { responseType: 'buffer' }).then((res) => res.body));
 
-				const image = dalleImageToBlob(response.data[0].url);
+				//const image = dalleImageToBlob(response.data[0].url);
+				const image = await got(response.data[0].url);
 				const formData = new FormData();
-				formData.append('file', image);
+				formData.append('file', image.body);
 
                 const imageURL =  await got.post('https://i.hotbear.org/upload', {
 					headers: {
