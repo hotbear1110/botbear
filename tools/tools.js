c21bf640c046de3234d4a0fb8a7e2e92bd5258ca
Mads Hauberg <mads095h@gmail.com>
2022-11-10 21:19:27 +0100

    Fixed transformNumbers

--- tools/tools.js
+++ tools/tools.js
@@ -694,29 +694,29 @@ exports.transformNumbers = function (message) {
 		message = numberConversion[message];
 		return message;
 	}
-	if ([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20].includes(message)) {
+	if (['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20'].includes(message)) {
 		let numberConversion = {
-			0: 'zero',
-			1: 'one',
-			2: 'two',
-			3: 'three',
-			4: 'four',
-			5: 'five',
-			6: 'six',
-			7: 'seven',
-			8: 'eight',
-			9: 'nine',
-			10: 'ten',
-			11: 'elven',
-			12: 'twelve',
-			13: 'thirteen',
-			14: 'fourteen',
-			15: 'fifteen',
-			16: 'sixteen',
-			17: 'seventeen',
-			18: 'eighteen',
-			19: 'nineteen',
-			20: 'twenty'
+			'0': 'zero',
+			'1': 'one',
+			'2': 'two',
+			'3': 'three',
+			'4': 'four',
+			'5': 'five',
+			'6': 'six',
+			'7': 'seven',
+			'8': 'eight',
+			'9': 'nine',
+			'10': 'ten',
+			'11': 'elven',
+			'12': 'twelve',
+			'13': 'thirteen',
+			'14': 'fourteen',
+			'15': 'fifteen',
+			'16': 'sixteen',
+			'17': 'seventeen',
+			'18': 'eighteen',
+			'19': 'nineteen',
+			'20': 'twenty'
 		};
 
 		message = numberConversion[message];
