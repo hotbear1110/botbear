0eb7d43edea37dd35fcc5b86f2cb6b9aa46aadb1
Mads Hauberg <mads095h@gmail.com>
2022-11-10 20:59:24 +0100

    Made deveval command

---
+++ commands/deveval.js
@@ -0,0 +1,27 @@
+module.exports = {
+	name: 'deveval',
+	ping: true,
+	description: 'This command will let you execute js code in the bot and make it return the result. Example: "bb deveval "lole ".repeat(10);',
+	permission: 2000,
+	cooldown: 3, //in seconds
+	category: 'Dev command',
+	opt_outable: false,
+	showDelay: false,
+	noBanphrase: false,
+	// eslint-disable-next-line no-unused-vars
+	execute: async (channel, user, input, perm, aliascommand) => {
+		try {
+			if (module.exports.permission > perm) {
+				return;
+			}
+            input = input.splice(2);
+			let msg = input.join(' ');
+            const js = eval(msg);
+            
+			return js;
+		} catch (err) {
+			console.log(err);
+			return 'FeelsDankMan Error';
+		}
+	}
+};
