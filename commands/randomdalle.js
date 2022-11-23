74e95c2fe2556c23624c5e00d6147dc71beca3b9
Mads Hauberg <mads095h@gmail.com>
2022-11-23 21:56:45 +0100

    Made a randomdalle command

---
+++ commands/randomdalle.js
@@ -0,0 +1,30 @@
+const sql = require('./../sql/index.js');
+
+module.exports = {
+	name: 'randomdalle',
+	ping: true,
+	description: 'DESCRIPTION',
+	permission: 100,
+	cooldown: 3, //in seconds
+	category: 'Gives you an random dalle image, another user has generated',
+	opt_outable: false,
+	showDelay: false,
+	noBanphrase: false,
+	// eslint-disable-next-line no-unused-vars
+	execute: async (channel, user, input, perm, aliascommand) => {
+		try {
+			if (module.exports.permission > perm) {
+				return;
+			}
+
+            const prompts = await sql.Query('SELECT * FROM Dalle',);
+
+            const prompt =  prompts[~~(Math.random() * prompts.length)];
+
+			return `Random dalle prompt | ID: ${prompt.ID} | User: ${prompt.User} | Prompt: ${prompt.Prompt} | Image: ${prompt.Image} | TimeStamp: ${prompt.Timestamp}`;
+		} catch (err) {
+			console.log(err);
+			return 'FeelsDankMan Error';
+		}
+	}
+};
