6fa8bfcc32bbc1b4babb009eff825f0072acdd53
Mads Hauberg <mads095h@gmail.com>
2022-11-29 22:22:22 +0100

    new  GPT-3 model

--- commands/ask.js
+++ commands/ask.js
@@ -32,7 +32,7 @@ module.exports = {
 			let msg = input.toString().replaceAll(',', ' ');
 			const prompt = `Q: ${msg}\nA:`;
 
-			const url = 'https://api.openai.com/v1/engines/text-davinci-002/completions';
+			const url = 'https://api.openai.com/v1/engines/text-davinci-003/completions';
 			const params = {
 				'prompt': prompt,
 				'max_tokens': 160,
