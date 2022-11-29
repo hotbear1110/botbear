0965396e18e1feba702683f83c3f91405c89e235
Mads Hauberg <mads095h@gmail.com>
2022-11-29 17:13:47 +0100

    Fixed output to not be a command

--- commands/age.js
+++ commands/age.js
@@ -1,3 +1,5 @@
+const regex = require('../tools/regex.js');
+
 module.exports = {
 	name: 'age',
 	ping: false,
@@ -16,6 +18,16 @@ module.exports = {
 			}
 
             let age = ~~(Math.random() * 102 + 18);
+
+
+			input[2].replace(regex.invisChar, '');
+
+            if (input[2].match(/[&|$|/|.|?|-]|\bkb\b|^\bmelon\b/g) && !input[2].match(/^[./]me /)) { // ignores &, $, kb, /, ., ?, !, - bot prefixes (. and / are twitch reserved prefixes)  
+				input[2] = '. ' + input[2].charAt(0) + '\u{E0000}' + input[2].substring(1);
+			}
+			if (input[2].match(/^!/g)) {
+				input[2] = '❗ ' + input[2].substring(1);
+			}
 
 			return `${input[2] ?? user.username} is ${age} years old.`;
 		} catch (err) {
