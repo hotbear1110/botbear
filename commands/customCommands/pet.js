eaba9289ebd8baecdc6450d3931886597aa777d3
Mads Hauberg <mads095h@gmail.com>
2022-12-02 15:52:20 +0100

    Made add, remove, ban and unban comands for pet

--- commands/customCommands/pet.js
+++ commands/customCommands/pet.js
@@ -19,19 +19,117 @@ module.exports = {
 				return;
 			}
 
-            let pets = await sql.Query('SELECT * FROM Yabbe_pet',);
+			switch(input[2]) {
+				case 'add': {
 
-			let users = [];
+					let isBanned = (await sql.Query('SELECT * FROM Yabbe_bans WHERE Command=? AND User=?', ['pet', user.username.toLowerCase()]));
 
-			pets.map(x => users.includes(x.User) ? false : users.push(x.User));
+					if (isBanned.length) {
+						return;
+					}
 
-            let user =  users[~~(Math.random() * users.length - 1)];
+					let id = (await sql.Query('SELECT ID FROM Yabbe_pet'));
 
-			pets = pets.filter(x => (x.User === user));
+					id = id[id.length - 1].ID + 1;
+					this.user = input.filter(x => x.toLowerCase().startsWith('user:'))[0]?.split(':')[1].slice(1, -1) ?? user.username;
 
-			let pet =  pets[~~(Math.random() * pets.length - 1)];
+					this.pet = input.filter(x => x.toLowerCase().startsWith('pet:'))[0]?.split(':')[1].slice(1, -1) ?? undefined;
 
-			return `Random pet image: User: ${pet.User} | Pet: ${pet.Pet} | Pet Name: ${pet.Pet_name} | Image: ${pet.Image} `;
+					this.name = input.filter(x => x.toLowerCase().startsWith('name:'))[0]?.split(':')[1].slice(1, -1) ?? undefined;
+
+					this.link = input.filter(x => x.toLowerCase().startsWith('link:'))[0]?.split(':')[1].slice(1, -1) ?? undefined;
+
+					if (!this.user || !this.pet || !this.name || !this.link) {
+						return 'Unexpected unput - Example: bb test add user:"NymN" pet:"Cat" name:"Apollo" link:"Link To "';
+					}
+
+					await sql.Query(`INSERT INTO Yabbe_pet 
+        			(ID, User, Pet, Pet_name, Image) 
+            			values 
+        			(?, ?, ?, ?, ?)`,
+				[id, this.user, this.pet, this.name, this.link]
+				);
+					return `Added new pet: ID: ${id} | User: ${this.user} | Pet: ${this.pet} | Pet Name: ${this.name} | Image: ${this.link} `;
+				}
+				case 'remove': {
+
+					const isMod = user.mod || user['user-type'] === 'mod';
+					const isBroadcaster = channel.toLowerCase() === user.username.toLowerCase();
+
+					if (!isMod && !isBroadcaster && perm < 2000) {
+						return 'Only mods can use this command';
+					}
+
+					if (!input[3])  {
+						return 'Please provide an id to be removed'; 
+					}
+
+					if (input[3].startsWith('-') || input[3] === '0') {
+						return '3rd input can\'t be negative or 0';
+	
+					}
+					let isnumber = !isNaN(input[3]);
+					if (!isnumber) {
+						return '3rd input should be a number';
+					}
+
+					const pet = await sql.Query('SELECT * FROM Yabbe_pet WHERE ID=?', [input[3]]);
+
+					if (!pet.length) {
+						return 'No pet entry with id: ' + input[3];
+					}
+
+					await sql.Query('DELETE FROM Yabbe_pet WHERE ID=?', [input[3]]);
+
+					return `Deleted pet image: ID: ${pet[0].ID} | User: ${pet[0].User} | Pet: ${pet[0].Pet} | Pet Name: ${pet[0].Pet_name} | Image: ${pet[0].Image} `;
+				}
+				case 'ban': {
+					let isBanned = (await sql.Query('SELECT * FROM Yabbe_bans WHERE Command=? AND User=?', ['pet', input[3].toLowerCase()]));
+
+					if (isBanned.length) {
+						return 'That user is already banned';
+					}
+
+					await sql.Query(`INSERT INTO Yabbe_bans 
+        			(Command, User) 
+            			values 
+        			(?, ?)`,
+				['pet', input[3].toLowerCase()]
+				);
+
+					return `@${input[3]} Is now banned`;
+				}
+				case 'unban': {
+					let isBanned = (await sql.Query('SELECT * FROM Yabbe_bans WHERE Command=? AND User=?', ['pet', input[3].toLowerCase()]));
+
+					if (!isBanned.length) {
+						return 'That user is not banned';
+					}
+
+					await sql.Query(`DELETE FROM Yabbe_bans 
+        			WHERE Command=? AND User=?`,
+				['pet', input[3].toLowerCase()]
+				);
+
+					return `@${input[3]} Is now unbanned`;
+				}
+				default: {
+					let pets = await sql.Query('SELECT * FROM Yabbe_pet',);
+
+					let users = [];
+		
+					pets.map(x => users.includes(x.User) ? false : users.push(x.User));
+		
+					let user =  users[~~(Math.random() * users.length - 1)];
+		
+					pets = pets.filter(x => (x.User === user));
+		
+					let pet =  pets[~~(Math.random() * pets.length - 1)];
+		
+					return `Random pet image: ID: ${pet.ID} | User: ${pet.User} | Pet: ${pet.Pet} | Pet Name: ${pet.Pet_name} | Image: ${pet.Image} `;
+				}
+			}
+
 		} catch (err) {
 			console.log(err);
 			return 'FeelsDankMan Error';
--- commands/test.js
+++ commands/test.js
@@ -1,28 +1,138 @@
-const { got } = require('./../got');
-const tools = require('../tools/tools.js');
 const sql = require('./../sql/index.js');
-let messageHandler = require('../tools/messageHandler.js').messageHandler;
 
 module.exports = {
 	name: 'test',
 	ping: true,
-	description: '123',
-	permission: 2000,
-	category: 'Dev command',
-	execute: async (channel, user, input, perm) => {
+	description: 'Shows an image of a pet from a random chatter',
+	permission: 100,
+	cooldown: 3, //in seconds
+	category: 'channelSpecific command',
+	opt_outable: false,
+	showDelay: false,
+	noBanphrase: true,
+	channelSpecific: true,
+	activeChannel: 'yabbe',
+	// eslint-disable-next-line no-unused-vars
+	execute: async (channel, user, input, perm, aliascommand) => {
 		try {
 			if (module.exports.permission > perm) {
 				return;
 			}
 
-			let test = ['test test tetst etstst es sdf sd', ' 453 543 543 5435 43 543 54334 5sdegdg ss sfsdfsf s  ', 'snfkjdsgbjhkfdjgndjkn sjlk gnsdjgs j'];
-			test.every((msg) => new messageHandler(channel, msg, true).newMessage());
+			switch(input[2]) {
+				case 'add': {
 
-			console.log(test);
-			return;
+					let isBanned = (await sql.Query('SELECT * FROM Yabbe_bans WHERE Command=? AND User=?', ['pet', user.username.toLowerCase()]));
+
+					if (isBanned.length) {
+						return;
+					}
+
+					let id = (await sql.Query('SELECT ID FROM Yabbe_pet'));
+
+					id = id[id.length - 1].ID + 1;
+					this.user = input.filter(x => x.toLowerCase().startsWith('user:'))[0]?.split(':')[1].slice(1, -1) ?? user.username;
+
+					this.pet = input.filter(x => x.toLowerCase().startsWith('pet:'))[0]?.split(':')[1].slice(1, -1) ?? undefined;
+
+					this.name = input.filter(x => x.toLowerCase().startsWith('name:'))[0]?.split(':')[1].slice(1, -1) ?? undefined;
+
+					this.link = input.filter(x => x.toLowerCase().startsWith('link:'))[0]?.split(':')[1].slice(1, -1) ?? undefined;
+
+					if (!this.user || !this.pet || !this.name || !this.link) {
+						return 'Unexpected unput - Example: bb test add user:"NymN" pet:"Cat" name:"Apollo" link:"Link To "';
+					}
+
+					await sql.Query(`INSERT INTO Yabbe_pet 
+        			(ID, User, Pet, Pet_name, Image) 
+            			values 
+        			(?, ?, ?, ?, ?)`,
+				[id, this.user, this.pet, this.name, this.link]
+				);
+					return `Added new pet: ID: ${id} | User: ${this.user} | Pet: ${this.pet} | Pet Name: ${this.name} | Image: ${this.link} `;
+				}
+				case 'remove': {
+
+					const isMod = user.mod || user['user-type'] === 'mod';
+					const isBroadcaster = channel.toLowerCase() === user.username.toLowerCase();
+
+					if (!isMod && !isBroadcaster && perm < 2000) {
+						return 'Only mods can use this command';
+					}
+
+					if (!input[3])  {
+						return 'Please provide an id to be removed'; 
+					}
+
+					if (input[3].startsWith('-') || input[3] === '0') {
+						return '3rd input can\'t be negative or 0';
+	
+					}
+					let isnumber = !isNaN(input[3]);
+					if (!isnumber) {
+						return '3rd input should be a number';
+					}
+
+					const pet = await sql.Query('SELECT * FROM Yabbe_pet WHERE ID=?', [input[3]]);
+
+					if (!pet.length) {
+						return 'No pet entry with id: ' + input[3];
+					}
+
+					await sql.Query('DELETE FROM Yabbe_pet WHERE ID=?', [input[3]]);
+
+					return `Deleted pet image: ID: ${pet[0].ID} | User: ${pet[0].User} | Pet: ${pet[0].Pet} | Pet Name: ${pet[0].Pet_name} | Image: ${pet[0].Image} `;
+				}
+				case 'ban': {
+					let isBanned = (await sql.Query('SELECT * FROM Yabbe_bans WHERE Command=? AND User=?', ['pet', input[3].toLowerCase()]));
+
+					if (isBanned.length) {
+						return 'That user is already banned';
+					}
+
+					await sql.Query(`INSERT INTO Yabbe_bans 
+        			(Command, User) 
+            			values 
+        			(?, ?)`,
+				['pet', input[3].toLowerCase()]
+				);
+
+					return `@${input[3]} Is now banned`;
+				}
+				case 'unban': {
+					let isBanned = (await sql.Query('SELECT * FROM Yabbe_bans WHERE Command=? AND User=?', ['pet', input[3].toLowerCase()]));
+
+					if (!isBanned.length) {
+						return 'That user is not banned';
+					}
+
+					await sql.Query(`DELETE FROM Yabbe_bans 
+        			WHERE Command=? AND User=?`,
+				['pet', input[3].toLowerCase()]
+				);
+
+					return `@${input[3]} Is now unbanned`;
+				}
+				default: {
+					let pets = await sql.Query('SELECT * FROM Yabbe_pet',);
+
+					let users = [];
+		
+					pets.map(x => users.includes(x.User) ? false : users.push(x.User));
+		
+					let user =  users[~~(Math.random() * users.length - 1)];
+		
+					pets = pets.filter(x => (x.User === user));
+		
+					let pet =  pets[~~(Math.random() * pets.length - 1)];
+		
+					return `Random pet image: ID: ${pet.ID} | User: ${pet.User} | Pet: ${pet.Pet} | Pet Name: ${pet.Pet_name} | Image: ${pet.Image} `;
+				}
+			}
+
 		} catch (err) {
 			console.log(err);
-			return `FeelsDankMan Sql error: ${err.sqlMessage}`;
+			return 'FeelsDankMan Error';
 		}
 	}
 };
