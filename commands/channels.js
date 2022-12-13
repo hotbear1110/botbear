97099163ad2d4ac58244bd279012dccd11a0cf9d
Mads Hauberg <mads095h@gmail.com>
2022-12-13 21:16:44 +0100

    Added markov channels from melon's api

--- commands/channels.js
+++ commands/channels.js
@@ -1,5 +1,6 @@
 const tools = require('../tools/tools.js');
 const sql = require('./../sql/index.js');
+const { got } = require('./../got');
 
 module.exports = {
 	name: 'channels',
@@ -22,10 +23,17 @@ module.exports = {
             const Streamers = await sql.Query('SELECT username FROM Streamers');
             
             let streamer_list = Streamers.map(x => x.username);
-            
+
+            const markovAPI = await got('https://magnolia.melon095.live/api/markov/list').json();
+            console.log(markovAPI);
+            let markov_channels = markovAPI.data.channels;
+
+            let markov_list = markov_channels.map(x => !streamer_list.includes(x.username) && x.username).filter(Boolean);
+
             streamer_list = streamer_list.toString().replaceAll(',', '\n');
-            
-            let hastebinlist = await tools.makehastebin(`All channels the bot is active in:\n\n${streamer_list}`);
+            markov_list = markov_list.toString().replaceAll(',', '\n');
+
+            let hastebinlist = await tools.makehastebin(`All channels the bot is active in:\n\n${streamer_list}\n\n\nExtra channels available for makov (If you try a channel not on the list, it will get added to the list and work after 100 messages are logged):\n\n${markov_list}`);
             
             return `All channels the bot is active in: ${hastebinlist}`;
 
