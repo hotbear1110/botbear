2956f9948e7d32cea2e044fdb3a93b714b01f9e2
Mads Hauberg <mads095h@gmail.com>
2023-01-05 11:12:49 +0100

    Made a channelspecific redeem command

---
+++ commands/customCommands/redeems.js
@@ -0,0 +1,31 @@
+const { got } = require('./../../got');
+
+module.exports = {
+	name: 'redeems',
+	ping: true,
+	description: 'Shows the latest channelpoint redeemed emotes',
+	permission: 100,
+	cooldown: 3, //in seconds
+	category: 'channelSpecific command',
+	opt_outable: false,
+	showDelay: false,
+	noBanphrase: false,
+	channelSpecific: true,
+	activeChannel: 'nymn',
+	// eslint-disable-next-line no-unused-vars
+	execute: async (channel, user, input, perm, aliascommand) => {
+		try {
+			if (module.exports.permission > perm) {
+				return;
+			}
+            const emotes = await got('https://bot-api.gempir.com/api/emotelog?channel=nymn&limit=100').json();
+
+            const response = emotes.filter(x => x.Type === 'seventv').map(x => `${x.EmoteCode} by ${x.AddedBy}`).slice(0, 5).join(', ');
+
+			return response;
+		} catch (err) {
+			console.log(err);
+			return 'FeelsDankMan Error';
+		}
+	}
+};
