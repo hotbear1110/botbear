a93762b2f331d236949755ed7542dd7cd2b1241b
Mads Hauberg <mads095h@gmail.com>
2023-01-25 15:53:42 +0100

    Added pagination

--- web/routes/pets.js
+++ web/routes/pets.js
@@ -3,10 +3,21 @@ module.exports = (function () {
     const sql = require('./../../sql/index.js');
 
     /* /suggestions */
-    router.get('/', async (req, res) => {
+    router.get('/(:page)?', async (req, res) => {
         let pets = await sql.Query('SELECT * FROM Yabbe_pet',);
 
-        res.render('pets', { pets: pets});
+        let perPage = 20;
+        let page = req.params.page || 1;
+
+        let count = pets.length;
+    
+        pets = pets.slice((perPage * page) - perPage, perPage * page);
+
+        res.render('pets', {
+            pets: pets,
+            current: page,
+            pages: Math.ceil(count / perPage)
+        });
     });
 
     return router;
