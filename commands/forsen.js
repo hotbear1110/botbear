cd7ece7751571a8bdba102d7634f3778d2b5e204
Mads Hauberg <77441913+hotbear1110@users.noreply.github.com>
2023-01-31 11:14:08 +0100

    forsen

---
+++ commands/forsen.js
@@ -0,0 +1,10 @@
+module.exports = {
+	name: 'forsen',
+	ping: true,
+	description: 'forsen',
+	permission: 100,
+	category: 'Random command',
+	noBanphrase: true,
+	// eslint-disable-next-line no-unused-vars
+	execute: async (channel, user, input, perm) => 'forsen'
+};
