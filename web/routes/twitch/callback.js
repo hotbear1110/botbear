91022dc68d5efd666a0da04c4a65f825bdc383e1
Mads Hauberg <mads095h@gmail.com>
2023-02-01 22:15:43 +0100

    Added cookies

--- web/routes/twitch/callback.js
+++ web/routes/twitch/callback.js
@@ -1,20 +1,22 @@
 require('dotenv').config();
 const querystring = require('querystring');
 const { got } = require('../../../got');
+const cookie = require('cookie');
 
 module.exports = (function () {
     const sql = require('../../../sql/index.js');
     const router = require('express').Router();
 
     const client_id = process.env.TWITCH_CLIENTID;
+    const client_secret = process.env.TWITCH_SECRET;
 
     /* /Callback */
     router.get('/', async (req, res) => {
         let code = res.query.code;
         let state = req.query.state || null;
 
+        
         console.log(res);
-      return router;
 
          if (state) {
           let userAuthOptions = {
@@ -43,19 +45,38 @@ module.exports = (function () {
           form: authOptions.form,
         }).json();
 
-          await sql.Query(`INSERT INTO Spotify 
-        			(state, uid, username) 
-            			values 
-        			(?, ?, ?)`,
-				[state, twitchRequest.data[0].id, twitchRequest.data[0].login]
-				);
-          state = current_state;
+        const hasID = await sql.Query('SELECT * FROM Spotify WHERE uid = ?', [twitchRequest.data[0].id]);
+        
+        if (hasID.length) {
+          let cookieToken = generateRandomString(255);
 
-                res.redirect('/resolved?' + 
-                querystring.stringify({
-                    state: state
-                  })
-                );
+          res.setHeader('Set-Cookie', cookie.serialize('token', String(cookieToken), {
+            httpOnly: true,
+            maxAge: 60 * 60 * 24 * 7 // 1 week
+          }));
+
+          await sql.Query('UPDATE Spotify SET  cookieToken = ? WHERE uid = ? ', [cookieToken, twitchRequest.data[0].id]);
+
+          res.redirect('../music');
+          return router;
+        }
+
+        let cookieToken = generateRandomString(255);
+
+        res.setHeader('Set-Cookie', cookie.serialize('token', String(cookieToken), {
+          httpOnly: true,
+          maxAge: 60 * 60 * 24 * 7 // 1 week
+        }));
+
+          await sql.Query(`INSERT INTO Spotify 
+        			(state, uid, username, cookieToken) 
+            			values 
+        			(?, ?, ?, ?)`,
+				[state, twitchRequest.data[0].id, twitchRequest.data[0].login, cookieToken]
+				);
+        
+        res.redirect('../music');
+
         } else {
             res.redirect('/#' +
             querystring.stringify({
@@ -65,3 +86,13 @@ module.exports = (function () {
 
     return router;
 })();
+
+const generateRandomString = function(length) {
+  let text = '';
+  let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
