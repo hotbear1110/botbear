bdef4d81fcb60e08a1ebb85df8e6b27196099188
Mads Hauberg <mads095h@gmail.com>
2023-02-01 23:19:41 +0100

    Update music.js

--- web/routes/music.js
+++ web/routes/music.js
@@ -1,12 +1,11 @@
 require('dotenv').config();
-const cookie = require('cookie');
 
 module.exports = (function () {
     const router = require('express').Router();
     
     /* /resolved */
     router.get('/', async (req, res) => {
-        let cookies = cookie.parse(req.headers.cookie || '');
+        let cookies = req.cookies || '';
 
         let cookieToken = cookies.cookieToken;
         
