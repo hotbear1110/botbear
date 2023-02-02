e808fe7b667a41b8db0f7468a0a6719cde0fb6fb
Mads Hauberg <77441913+hotbear1110@users.noreply.github.com>
2023-02-02 10:46:34 +0100

    Fetching twitch logo

--- web/routes/music.js
+++ web/routes/music.js
@@ -1,15 +1,23 @@
 require('dotenv').config();
+const router = require('express').Router();
+const sql = require('./../../sql/index.js');
+const { got } = require('./../../got');
 
 module.exports = (function () {
-    const router = require('express').Router();
-    
     /* /resolved */
     router.get('/', async (req, res) => {
         let cookies = req.cookies || '';
 
         let cookieToken = cookies.token;
-        
-        res.render('music', { cookieToken: cookieToken });
+
+        let [userInfo] = await sql.Query('SELECT * FROM Spotify',);
+
+        if (userInfo.length) {
+            const [getImage] = got(`https://api.ivr.fi/v2/twitch/user?id=${userInfo.id}`).json();
+            userInfo['logo'] = getImage.logo;
+        }
+
+        res.render('music', { cookieToken: userInfo.length, userInfo: userInfo });
     });
 
     return router;
