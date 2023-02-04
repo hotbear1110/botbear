9ace1d8b2340faaebe66f7931e94ee6a31e9deab
Mads Hauberg <mads095h@gmail.com>
2023-02-04 21:59:32 +0100

    Changed scope

--- web/routes/spotify/login.js
+++ web/routes/spotify/login.js
@@ -10,7 +10,7 @@ module.exports = (function () {
     /* /Login */
     router.get('/', async (req, res) => {
         let state = generateRandomString(16);
-        let scope = 'user-read-playback-state user-read-currently-playing user-read-private user-modify-playback-state';
+        let scope = 'user-read-playback-state user-read-private user-modify-playback-state';
       
         res.redirect('https://accounts.spotify.com/authorize?' +
           querystring.stringify({
