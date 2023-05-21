57966208b6be36b4909f8f4c04d795b53e785ccb
Mads Hauberg <mads095h@gmail.com>
2023-05-21 21:08:54 +0200

    removed unused code

--- commands/thumbnail.js
+++ commands/thumbnail.js
@@ -84,14 +84,3 @@ module.exports = {
 		}
 	}
 };
-
-
-const generateRandomString = function(length) {
-    let text = '';
-    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  
-    for (let i = 0; i < length; i++) {
-      text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return text;
-  };
