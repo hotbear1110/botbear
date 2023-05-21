71405491722e671ffa03b348f0693a66d39ec01d
Mads Hauberg <mads095h@gmail.com>
2023-05-21 12:09:45 +0200

    Update tools.js

--- tools/tools.js
+++ tools/tools.js
@@ -711,7 +711,7 @@ exports.transformNumbers = function (message) {
 			'twenty': '20'
 		};
 
-		message = numberConversion[message];
+		message = numberConversion[message.toLowerCase()];
 		return message;
 	}
 	if (['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20'].includes(message)) {
