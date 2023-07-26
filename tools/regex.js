b48874d87f03245fdf5b18ef4e3c8c4920adfb87
Mads Hauberg <mads095h@gmail.com>
2023-07-26 21:37:19 +0200

    Trying new regex, so it wont catch decimal numbers
    Thanks @hamnomc
    
    Co-Authored-By: Hamnomc <79751180+hamnomc@users.noreply.github.com>

--- tools/regex.js
+++ tools/regex.js
@@ -3,5 +3,5 @@
 // eslint-disable-next-line no-misleading-character-class
 exports.racism = new RegExp(/(?:(?:\b(?<![-=\.])|monka)(?:[Nnñ]|[Ii7]V)|[\/|]\\[\/|])[\s\.]*?[liI1y!j\/|]+[\s\.]*?(?:[GgbB6934Q🅱qğĜƃ၅5\*][\s\.]*?){2,}(?!arcS|l|Ktlw|ylul|ie217|64|\d? ?times)/);
 exports.invisChar = new RegExp(/[\u034f\u2800\u{E0000}\u180e\ufeff\u2000-\u200d\u206D]/gu);
-exports.URL = /(https?\:\/\/)?(\w+\.?\w+)(\.\w+)(\.\w+)?(\/\S+)?/gim;
+exports.URL = /(https?\:\/\/)?(\w+\.?\w+)(\.[^0-9]+)(\.\w+)?(\/\S+)?/gim;
 exports.HIDDEN_CHARACTERS = new RegExp(/[\uDB40-\uDC00\u034f\u2800\u{E0000}\u180e\ufeff\u2000-\u200d\u206D]/g);
