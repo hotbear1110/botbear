b7c521f3b59a23e38a1fe9676b9dd59077887d54
Mads Hauberg <77441913+hotbear1110@users.noreply.github.com>
2023-08-22 22:31:57 +0200

    Update 2_update.sql

--- sql/migrations/2_update.sql
+++ sql/migrations/2_update.sql
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS `Spotify` (
   `access_token` varchar(255) NOT NULL DEFAULT '',
   `refresh_token` varchar(255) NOT NULL DEFAULT '',
   `expires_in` varchar(255) DEFAULT NULL,
-  'opt_in' varchar(255) DEFAULT 'false'
+  `opt_in` varchar(255) DEFAULT 'false'
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `Yabbe_bans` (
