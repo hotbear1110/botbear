765a3880e837a7ad5d04bc95b8ddfffd808ac231
Mads Hauberg <s224307@dtu.dk>
2023-12-26 20:38:15 +0100

    Removed unused/bad commands
    Removed a bunch of commands that aren't used that much, and also some annoying ones that users have been complaining about (random pinging)

--- commands/trihard.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	name: 'trihard',
-	ping: true,
-	description: 'Says TriHard',
-	permission: 100,
-	category: 'Random command',
-	// eslint-disable-next-line no-unused-vars
-	execute: async (channel, user, input, perm) => 'TriHard'
-};
