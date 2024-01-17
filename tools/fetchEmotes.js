90b3f03bdea2cb0f26ed5d0ff5e031f2a348d6f6
Mads Hauberg <s224307@dtu.dk>
2024-01-17 22:13:43 +0100

    changed from foreach to reduce

--- tools/fetchEmotes.js
+++ tools/fetchEmotes.js
@@ -8,22 +8,28 @@ exports.STV_emotes = new Promise(async(Resolve) => {
         try {
             const STV_api = await got(`https://7tv.io/v3/users/twitch/${user.uid}`).json();
 
-            const emote_list = []
-            STV_api.emote_set.emotes.forEach(emote => {
-                emote_list.push(
-                    {
-                        "name": emote.name,
-                        "id": emote.id,
-                        "time_added": emote.timestamp,
-                        "uploader": emote.data.owner.username,
-                        "platform": "7TV"
-                    }
-                )
-            });
-    
-            await sql.Query('UPDATE Streamers SET emote_list=? WHERE uid=?', [JSON.stringify(emote_list), user.uid]);
+            const emote_list = [];
+
+
+            if (STV_api.emote_set.emotes.length) {
+                emote_list = STV_api.emote_set.emotes.reduce(
+                    (emote_list, emote) => { 
+                        emote_list.push(
+                        {
+                            "name": emote.name,
+                            "id": emote.id,
+                            "time_added": emote.timestamp,
+                            "uploader": (emote.data.owner?.username !== undefined) ? emote.data.owner.username : 'Unknown',
+                            "platform": "7TV"
+                        }
+                        ); return chatters;
+                    }, []);
+        
+                await sql.Query('UPDATE Streamers SET emote_list=? WHERE uid=?', [JSON.stringify(emote_list), user.uid]);
+            }
+
         } catch(error) {
-            console.log(error)
+            console.log(error);
         }
 
     }
