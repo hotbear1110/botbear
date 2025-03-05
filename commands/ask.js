4740579bb78a3ef5ccb385ef2f29d33fa7e483b6
Mads Hauberg <mail@hotbear.org>
2025-03-05 17:53:24 +0100

    Update ask.js

--- commands/ask.js
+++ commands/ask.js
@@ -129,7 +129,7 @@ module.exports = {
 					'messages': [
 						{
 							'role': 'system',
-							'content': `short answer, no more than 300 letters, symbols and spaces total`
+							'content': `short response, response no more than 300 letters, symbols and spaces total`
 						},
 						{
 							'role': 'user',
