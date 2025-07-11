105d19f47bf307b318a2e4317f56824872d12e7e
Mads Hauberg <mail@hotbear.org>
2025-07-11 11:57:01 +0200

    Update connect.js

--- connect/connect.js
+++ connect/connect.js
@@ -17,30 +17,30 @@ exports.setupChannels = new Promise(async (Resolve) => {
 	console.log(`Imported channels from database: ${channelOptions}`);
 
 	const old_refresh_token = (await sql.Query('SELECT refresh_token FROM Auth_users WHERE uid = ?', [process.env.TWITCH_UID]))[0].refresh_token;
-	let refresh;
+
 	try {
-		refresh = await got.post('https://id.twitch.tv/oauth2/token?' +
+		const refresh = await got.post('https://id.twitch.tv/oauth2/token?' +
 			querystring.stringify({
 			  client_id: client_id,
 			  client_secret: client_secret,
 			  grant_type: 'refresh_token',
 			  refresh_token: old_refresh_token
 			}));
-	} catch (err) {
-		console.log(err);
-	}
 
-	console.log(await refresh);
+			console.log(await refresh);
 
 	
-	if (!refresh.error) {
-		await sql.Query('UPDATE Auth_users SET access_token = ?, refresh_token = ?, expires_in = ? WHERE uid = ?', [refresh.access_token, refresh.refresh_token, refresh.expires_in, process.env.TWITCH_UID]);
-
-		password = refresh.access_token;
-		Resolve();
-		return;
+			if (!refresh.error) {
+				await sql.Query('UPDATE Auth_users SET access_token = ?, refresh_token = ?, expires_in = ? WHERE uid = ?', [refresh.access_token, refresh.refresh_token, refresh.expires_in, process.env.TWITCH_UID]);
+		
+				password = refresh.access_token;
+				Resolve();
+				return;
+			}
+	} catch (err) {
+		console.log(err);
+		throw('');
 	}
-	throw('');
 });
 
 exports.TMISettings = {
