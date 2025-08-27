7832f977ca035c7ec4a4bd97a76171096d523e48
Mads Toft Hauberg <77441913+hotbear1110@users.noreply.github.com>
2025-08-27 11:05:11 +0200

    Update thumbnail.js

--- commands/thumbnail.js
+++ commands/thumbnail.js
@@ -65,10 +65,12 @@ module.exports = {
 				return '[OFFLINE] | VOD Thumbnail: ' + thumbnail;
 			}
 
+			thumbnail += '?cum';
+
 			const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
 			for (let i = 0; i < 5; i++) {
-				thumbnail += '?' + possible.charAt(Math.floor(Math.random() * possible.length));
+				thumbnail += possible.charAt(Math.floor(Math.random() * possible.length));
 			}
 						
 			return thumbnail;
